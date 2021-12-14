91a68ac753436a0b454d56758ac5d981fdfa7fe5
Dane Grant <danecando@gmail.com>
2021-12-14 15:49:49 -0500

    Working initial create CLI prototype

--- packages/create-starter/src/index.ts
+++ packages/create-starter/src/index.ts
@@ -1,6 +1,8 @@
-import { bold, gray, green, red } from 'kleur/colors';
+import path from 'path';
+import fs from 'fs/promises';
+import { bold, gray, green, red, cyan } from 'kleur/colors';
 import prompts from 'prompts';
-import degit from 'degit';
+import degit from 'tiged';
 import starters from './starters';
 
 export async function main() {
@@ -17,7 +19,6 @@ export async function main() {
       type: 'text',
       name: 'name',
       message: 'What is the name of your project?',
-      validate: (value) => (value.length > 0 ? true : 'Please enter a name'),
     },
   ]);
 
@@ -25,19 +26,31 @@ export async function main() {
     process.exit(1);
   }
 
-  const templateTarget = `thisdot/starter.dev/starters/${options.kit}#latest ${options.name}`;
+  const repoPath = `thisdot/starter.dev/starters/${options.kit}`;
+  const destPath = path.join(process.cwd(), options.name);
 
-  const emitter = degit(templateTarget, {
+  const emitter = degit(repoPath, {
     cache: false,
     force: true,
     verbose: false,
+    mode: 'git', // @todo: change to 'tar' when repo is public
   });
 
   try {
-    console.log(`${green(`>`)} ${gray(`Copying project files...`)}`);
-    await emitter.clone(process.cwd());
+    console.log(`${green(`>`)} ${gray(`Downloading starter kit...`)}`);
+    await emitter.clone(destPath);
   } catch (err: any) {
     console.error(red(err.message));
     process.exit(1);
   }
+
+  const packageJSON = JSON.parse(await fs.readFile(path.join(destPath, 'package.json'), 'utf8'));
+  packageJSON.name = options.name;
+  packageJSON.version = '0.1.0';
+  await fs.writeFile(path.join(destPath, 'package.json'), JSON.stringify(packageJSON, null, 2));
+
+  console.log(bold(green('✔') + ' Done!'));
+  console.log('\nNext steps:');
+  console.log(` ${bold(cyan(`cd ${options.name}`))}`);
+  console.log(` ${bold(cyan('npm install'))} (or pnpm install, yarn, etc)`);
 }
