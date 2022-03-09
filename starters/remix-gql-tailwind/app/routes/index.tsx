f6110a605c9ff422b102b59f600a81b57a785191
vyktoremario <umehvictormario@gmail.com>
2022-03-09 21:25:08 +0100

    Added the homePage route

--- starters/remix-gql-tailwind/app/routes/index.tsx
+++ starters/remix-gql-tailwind/app/routes/index.tsx
@@ -1,32 +1,5 @@
-export default function Index() {
-  return (
-    <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li className=" text-2xl text-green-300">
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import { Counter } from '~/components/Counter';
+
+export default function HomePage() {
+  return <Counter />;
 }
