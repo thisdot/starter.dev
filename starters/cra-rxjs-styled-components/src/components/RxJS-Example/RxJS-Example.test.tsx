48d01818d7a791eee5948ee3b77202ee4f27f7e8
jdwilkin4 <jwilkin4@hotmail.com>
2022-03-11 08:56:34 -0800

    feat: adding testing for mocking rxjs fetch call

--- starters/cra-rxjs-styled-components/src/components/RxJS-Example/RxJS-Example.test.tsx
+++ starters/cra-rxjs-styled-components/src/components/RxJS-Example/RxJS-Example.test.tsx
@@ -1,1 +1,19 @@
-import { RxJSExample } from './RxJS-Example';
+import fetch from 'jest-fetch-mock';
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+test('returns a string result', () => {
+  fetch.mockResponseOnce('Hello, from This Dot Labs!');
+  const onResponse = jest.fn();
+  const onError = jest.fn();
+
+  return fetch('https://api.starter.dev/hello?greeting=from This Dot Labs!')
+    .then(onResponse)
+    .catch(onError)
+    .finally(() => {
+      expect(onResponse).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+    });
+});
