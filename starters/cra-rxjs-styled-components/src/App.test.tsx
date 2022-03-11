345b2408a7d7970092bd2d265411a0ce16e5ea30
jdwilkin4 <jwilkin4@hotmail.com>
2022-03-11 13:14:12 -0800

    feat: testing routes for rxjs and counter examples

--- starters/cra-rxjs-styled-components/src/App.test.tsx
+++ starters/cra-rxjs-styled-components/src/App.test.tsx
@@ -1,6 +1,8 @@
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Counter } from './components/Counter';
+import { RxJSExample } from './components/RxJS-Example';
 
 test('full app rendering/navigating', () => {
   render(<App />, { wrapper: MemoryRouter });
@@ -10,4 +12,18 @@ test('full app rendering/navigating', () => {
       /Create React App, RxJS and styled-components Starter kit/i
     )
   ).toBeInTheDocument();
+});
+
+test('testing rendering/navigating for counter component', () => {
+  render(<Counter />, { wrapper: MemoryRouter });
+
+  expect(
+    screen.getByText(/Increment, Decrement and Reset Button Examples/i)
+  ).toBeInTheDocument();
+});
+
+test('testing rendering/navigating for RxJS component', () => {
+  render(<RxJSExample />, { wrapper: MemoryRouter });
+
+  expect(screen.getByText(/RxJS Fetch Data from API/i)).toBeInTheDocument();
 });
