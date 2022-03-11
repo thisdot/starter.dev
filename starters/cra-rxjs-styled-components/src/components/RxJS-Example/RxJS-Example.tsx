e6dd9b41ed00d2b94f24a036af5b189b57a6fa7b
jdwilkin4 <jwilkin4@hotmail.com>
2022-03-11 07:09:01 -0800

    feat: adding rxjs fetch call

--- starters/cra-rxjs-styled-components/src/components/RxJS-Example/RxJS-Example.tsx
+++ starters/cra-rxjs-styled-components/src/components/RxJS-Example/RxJS-Example.tsx
@@ -3,11 +3,24 @@ import {
   HomeLinkDiv,
   ReturnHomeLink,
 } from './RxJS-Example.styles';
+import { useState, useEffect } from 'react';
+import { fromFetch } from 'rxjs/fetch';
 
 export const RxJSExample = () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    const subscription = fromFetch(
+      'https://api.starter.dev/hello?greeting=jess'
+    ).subscribe((response) => response.text().then((data) => setData(data)));
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <RxJSExampleContainer>
       <h1>RxJS Fetch Data from API</h1>
+      <p>Message: {data}</p>
       <HomeLinkDiv>
         <ReturnHomeLink to="/">Return Home</ReturnHomeLink>
       </HomeLinkDiv>
