4239eb79152bbd4e498bf56131cd5f16df9e8fe8
vyktoremario <umehvictormario@gmail.com>
2022-03-21 15:03:48 +0100

    refactored

--- packages/website/src/icons/TechnologiesGridIcon.tsx
+++ packages/website/src/icons/TechnologiesGridIcon.tsx
@@ -1,10 +1,6 @@
-import { Props } from './types';
-
-export function TechnologiesGridIcon({ size }: Props) {
+export function TechnologiesGridIcon() {
   return (
     <svg
-      width={size}
-      height={size}
       viewBox="0 0 732 316"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
