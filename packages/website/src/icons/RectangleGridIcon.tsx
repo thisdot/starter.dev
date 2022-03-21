7fb7036678e38a4375a59fafbde365cdf138f3be
vyktoremario <umehvictormario@gmail.com>
2022-03-21 15:13:18 +0100

    refactoring the svg

--- packages/website/src/icons/RectangleGridIcon.tsx
+++ packages/website/src/icons/RectangleGridIcon.tsx
@@ -7,23 +7,23 @@ export function RectangleGridIcon() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="168" y="176" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect y="176" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="256" y="76" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="88" y="76" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="344" y="-24" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="176" y="-24" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="256" y="176" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="428" y="176" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="84" y="176" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="344" y="76" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="516" y="76" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="172" y="76" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="432" y="-24" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="260" y="-24" width="64" height="64" rx="8" class="fill-gray-200 dark:fill-gray-750" />
-      <rect x="344" y="176" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="432" y="76" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
-      <rect x="520" y="-24" width="64" height="64" rx="8" class="fill-gray-100 dark:fill-gray-800" />
+      <rect x="168" y="176" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect y="176" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="256" y="76" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="88" y="76" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="344" y="-24" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="176" y="-24" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="256" y="176" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="428" y="176" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="84" y="176" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="344" y="76" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="516" y="76" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="172" y="76" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="432" y="-24" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="260" y="-24" width="64" height="64" rx="8" className="fill-gray-200 dark:fill-gray-750" />
+      <rect x="344" y="176" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="432" y="76" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
+      <rect x="520" y="-24" width="64" height="64" rx="8" className="fill-gray-100 dark:fill-gray-800" />
     </svg>
   );
 }
