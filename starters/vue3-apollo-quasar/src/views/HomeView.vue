54e9f83c513d1260d17361396ac4846c208adcdd
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-06 15:37:40 +0100

    Feat: Created a vue3 application completed

--- starters/vue3-apollo-quasar/src/views/HomeView.vue
+++ starters/vue3-apollo-quasar/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to Your vue3-apollo-quasar starter kit" />
   </div>
 </template>
 
