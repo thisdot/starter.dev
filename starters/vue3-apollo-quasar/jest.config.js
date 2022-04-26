c8708faac740b1fffea01541c9612e092c779339
Allan Jeremy <aj.dev254@gmail.com>
2022-04-26 15:50:56 +0300

    [Vue3-Apollo-Quasar] config: Added TS support in tests

--- starters/vue3-apollo-quasar/jest.config.js
+++ starters/vue3-apollo-quasar/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  moduleFileExtensions: ['ts', 'js', 'vue'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
 };
