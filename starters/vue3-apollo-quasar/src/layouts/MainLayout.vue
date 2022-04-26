51f818185bdb797819f563e21f9e773c13b921dc
Allan Jeremy <aj.dev254@gmail.com>
2022-04-26 10:42:02 +0300

    [Vue - Cleanup] Removed unused components

--- starters/vue3-apollo-quasar/src/layouts/MainLayout.vue
+++ starters/vue3-apollo-quasar/src/layouts/MainLayout.vue
@@ -20,12 +20,6 @@
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
       </q-list>
     </q-drawer>
 
@@ -37,7 +31,6 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
@@ -87,9 +80,7 @@ const linksList = [
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink,
-  },
+  components: {},
 
   setup() {
     const leftDrawerOpen = ref(false);
