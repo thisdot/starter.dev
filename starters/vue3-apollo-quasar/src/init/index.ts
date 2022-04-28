b5de713d52ff94e2bca987da461548cef02c0527
Jerry Hogan <jerry.hogan@thisdot.co>
2022-04-28 12:18:06 +0100

    Vue3 + Apollo + Quasar - Added Apollo

---
+++ starters/vue3-apollo-quasar/src/init/index.ts
@@ -0,0 +1,5 @@
+/* 
+    This file is simply meant to be an aggregator of all init functions.
+    This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
+*/
+export { setupGraphQL } from './setupGraphQL';
