83820d11428023f4d80aaa3404f50419f980ffb0
Ian Mungai <iansam74@gmail.com>
2022-04-29 16:16:31 +0300

    feat: add apollo

--- starters/angular-apollo-tailwind/src/app/app.module.ts
+++ starters/angular-apollo-tailwind/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
