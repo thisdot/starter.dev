b3704fda024761d2d350405d3610e4f85cdcb174
Ian Mungai <iansam74@gmail.com>
2022-05-03 09:59:38 +0300

    Hello Service Test

--- starters/angular-apollo-tailwind/src/app/fetch-example/hello.service.spec.ts
+++ starters/angular-apollo-tailwind/src/app/fetch-example/hello.service.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { HelloService } from './hello.service';
 
 describe('HelloService', () => {
-  let service: HelloService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let helloService: HelloService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HelloService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    helloService = new HelloService(httpClientSpy);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be return hello message', (done: DoneFn) => {
+    const expectedHelloMessage: string = "angular-apollo-tailwind starter.dev!"
+  
+    httpClientSpy.get.and.returnValue(of(expectedHelloMessage));
+  
+    helloService.getHelloMessage(expectedHelloMessage).subscribe({
+      next: helloMessage => {
+        expect(helloMessage)
+          .withContext('expected hello message')
+          .toEqual(`${expectedHelloMessage}`);
+          // .toEqual(`Hello, "${expectedHelloMessage}"`);
+        done();
+      },
+      error: done.fail
+    });
+
+    expect(httpClientSpy.get.calls.count())
+      .withContext('one call')
+      .toBe(1);
   });
+
 });
