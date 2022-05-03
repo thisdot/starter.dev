0aabcab52ced940a28e935ac21d2812e6af755f0
Ian Mungai <iansam74@gmail.com>
2022-05-03 15:10:27 +0300

    graphql setup

--- starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.component.ts
+++ starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HelloService } from './hello.service';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Apollo, gql } from 'apollo-angular';
+
+export const GET_GREETING = gql`
+  query HelloQuery($greeting: String!) {
+    hello(greeting: $greeting)
+  }
+`;
 
 @Component({
   selector: 'app-fetch-example',
@@ -9,15 +16,42 @@ import { HelloService } from './hello.service';
 })
 export class FetchExampleComponent implements OnInit {
 
-  private message: string = 'angular-apollo-tailwind starter.dev!'
-  helloMessage$!: Observable<any>;
+  private message: string = 'Angular + Apollo + Tailwind starter.dev'
+  greetingMessage$!: Observable<any>;
 
   constructor(
-    private helloService: HelloService
+    private apollo: Apollo
   ) { }
 
   ngOnInit(): void {
-    this.helloMessage$ = this.helloService.getHelloMessage(this.message)
+    this.greetingMessage$ = this.getGreetingMessage(this.message)
+  }
+
+  getGreetingMessage(message: string): Observable<any> {
+    return this.apollo.watchQuery<any>({
+      query: GET_GREETING,
+      variables: {
+        greeting: message
+      }
+    })
+    .valueChanges
+    .pipe(
+      map(({data}) => data.hello),
+      catchError(this.handleError<string>('getGreetingMessage', ''))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.log('operation:', operation);
+
+      // send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
