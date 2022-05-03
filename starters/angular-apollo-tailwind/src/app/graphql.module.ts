c21c168e98a048ebdd15a4da5834eadc49732061
Ian Mungai <iansam74@gmail.com>
2022-05-03 15:05:38 +0300

    graphql setup

--- starters/angular-apollo-tailwind/src/app/graphql.module.ts
+++ starters/angular-apollo-tailwind/src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'https://api.starter.dev/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
