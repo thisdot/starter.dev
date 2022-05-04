e4d2ff247375bb34f3fd9ffaa621381d0f7df23b
Ian Sam Mungai <iansam74@gmail.com>
2022-05-04 08:21:03 +0300

    [Angular-Apollo-Tailwind]  Create Fetch component (#155)
    * graphql setup
    
    * testing component
    
    * storybook story for fetch example
    
    * lint project

--- starters/angular-apollo-tailwind/src/app/app.module.ts
+++ starters/angular-apollo-tailwind/src/app/app.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FetchExampleComponent } from './fetch-example/fetch-example.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    GraphQLModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, FetchExampleComponent],
+    imports: [BrowserModule, GraphQLModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- starters/angular-apollo-tailwind/src/stories/Header.stories.ts
+++ starters/angular-apollo-tailwind/src/stories/Header.stories.ts
@@ -6,29 +6,29 @@ import Button from './button.component';
 import Header from './header.component';
 
 export default {
-  title: 'Example/Header',
-  component: Header,
-  decorators: [
-    moduleMetadata({
-      declarations: [Button],
-      imports: [CommonModule],
-    }),
-  ],
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/angular/configure/story-layout
-    layout: 'fullscreen',
-  },
+    title: 'Example/Header',
+    component: Header,
+    decorators: [
+        moduleMetadata({
+            declarations: [Button],
+            imports: [CommonModule],
+        }),
+    ],
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/angular/configure/story-layout
+        layout: 'fullscreen',
+    },
 } as Meta;
 
 const Template: Story<Header> = (args: Header) => ({
-  props: args,
+    props: args,
 });
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  user: {
-    name: 'Jane Doe',
-  },
+    user: {
+        name: 'Jane Doe',
+    },
 };
 
 export const LoggedOut = Template.bind({});
