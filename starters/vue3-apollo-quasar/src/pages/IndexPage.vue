aa52688d4a3bc94a3f3e02ec2ab088275c429c93
Allan Jeremy <aj.dev254@gmail.com>
2022-05-05 13:14:45 +0300

    [Cleanup] Updated home page to contain links to the counter and api components Also updated routes for the same

--- starters/vue3-apollo-quasar/src/pages/IndexPage.vue
+++ starters/vue3-apollo-quasar/src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <p v-if="loading">fetching user from github...</p>
-
-    <div v-else>
-      <q-card class="my-card">
-        <q-img :src="data.avatarUrl">
-          <div class="absolute-bottom text-subtitle2 text-center">
-            <p>
-              {{ data.name }}
-            </p>
-          </div>
-        </q-img>
-      </q-card>
+    <div class="text-center">
+      <p>
+        <router-link class="text-primary" to="/counter">
+          See example Counter component
+        </router-link>
+      </p>
+      <p>
+        <router-link class="text-primary" to="/api-example">
+          See example API component
+        </router-link>
+      </p>
     </div>
   </q-page>
 </template>
@@ -19,44 +18,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import { useQuery, useResult } from '@vue/apollo-composable';
-import gql from 'graphql-tag';
-
 export default defineComponent({
   name: 'IndexPage',
-  setup() {
-    const USER_PROFILE_QUERY = gql`
-      query ($username: String!) {
-        user(login: $username) {
-          status {
-            emojiHTML
-            message
-            __typename
-          }
-          name
-          login
-          bio
-          company
-          avatarUrl
-        }
-      }
-    `;
-
-    const { result, loading } = useQuery(USER_PROFILE_QUERY, {
-      username: 'hdjerry',
-    });
-
-    const data = useResult(result, [], ({ user }) => ({
-      ...user,
-    }));
-
-    return { data, loading };
-  },
 });
 </script>
-
-<style lang="scss" scoped>
-.my-card {
-  width: 250px;
-}
-</style>
