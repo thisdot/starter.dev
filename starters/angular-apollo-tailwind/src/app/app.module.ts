3d3cb51340a4517a7c719f1e084e904a20b719d8
Ian Sam Mungai <iansam74@gmail.com>
2022-05-05 07:32:05 +0300

    [Angular-Apollo-Tailwind]: Home Page and Routing (#159)
    * feat: home page and routing
    
    * change counter to counter-example

--- starters/angular-apollo-tailwind/src/app/app.module.ts
+++ starters/angular-apollo-tailwind/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FetchExampleComponent } from './fetch-example/fetch-example.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterExampleComponent } from './counter-example/counter-example.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-    declarations: [AppComponent, FetchExampleComponent, CounterComponent],
-    imports: [BrowserModule, GraphQLModule, HttpClientModule],
+    declarations: [AppComponent, FetchExampleComponent, CounterExampleComponent],
+    imports: [BrowserModule, GraphQLModule, HttpClientModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
