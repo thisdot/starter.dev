958c4ec2fb2e0314996c8ab7154759893a8c5a94
Ian Sam Mungai <iansam74@gmail.com>
2022-05-05 07:04:43 +0300

    [Angular-Apollo-Tailwind] Counter component (#158)
    * fix: lint issue storybook
    
    * counter setup
    
    * feat: counter example
    
    * fix small issue
    
    * remove commented test

--- starters/angular-apollo-tailwind/src/app/app.module.ts
+++ starters/angular-apollo-tailwind/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FetchExampleComponent } from './fetch-example/fetch-example.component';
+import { CounterComponent } from './counter/counter.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [AppComponent, FetchExampleComponent],
+    declarations: [AppComponent, FetchExampleComponent, CounterComponent],
     imports: [BrowserModule, GraphQLModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
