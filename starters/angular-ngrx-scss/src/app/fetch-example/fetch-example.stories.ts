2a5d26019b559ac05d9b1c807d0ddee0e4c68151
LindaT <linda@thisdot.co>
2022-05-12 17:48:14 -0500

    feat: created story for fetch example

--- starters/angular-ngrx-scss/src/app/fetch-example/fetch-example.stories.ts
+++ starters/angular-ngrx-scss/src/app/fetch-example/fetch-example.stories.ts
@@ -1,30 +1,43 @@
-import { Store } from '@ngrx/store';
-import { Story, Meta, componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+import { Store, StoreModule } from '@ngrx/store';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { of } from 'rxjs';
+import { greetingReducer } from '../state/greeting/greeting.reducer';
 import { FetchExampleComponent } from './fetch-example.component';
-
 export default {
   title: 'Fetch Example',
   component: FetchExampleComponent,
   decorators: [
     moduleMetadata({
+      imports: [StoreModule.forRoot(greetingReducer)],
       declarations: [FetchExampleComponent],
       providers: [Store],
     }),
-    componentWrapperDecorator((story) => `<div style="margin: 3em">${story}</div>`),
   ],
 } as Meta;
 
 const Template: Story<FetchExampleComponent> = (args: FetchExampleComponent) => ({
   props: {
     ...args,
-    greeting$: '',
-    error$: '',
-    isLoading$: false,
   },
 });
 
-export const Loading = Template.bind({});
-Loading.args = {
+export const LoadingGreeting = Template.bind({});
+LoadingGreeting.args = {
   isLoading$: of(true),
+  greeting$: of(''),
+  error$: of(''),
+};
+
+export const MessageWithGreeting = Template.bind({});
+MessageWithGreeting.args = {
+  isLoading$: of(false),
+  greeting$: of('Hello, angular-ngrx-scss starter.dev!'),
+  error$: of(''),
+};
+
+export const MessageNoGreeting = Template.bind({});
+MessageNoGreeting.args = {
+  isLoading$: of(false),
+  greeting$: of('Hello, there'),
+  error$: of(''),
 };
