670c78e4fa05cb142174f7871a6b8aa246a44135
Dane Grant <danecando@gmail.com>
2022-05-23 11:55:33 -0400

    fix(website): fixes generated kit pages path

--- packages/website/scripts/generate-kit-pages.js
+++ packages/website/scripts/generate-kit-pages.js
@@ -45,7 +45,7 @@ ${markdown}`;
 
 (async () => {
   const repoPath = getRepoRootPath();
-  const kitPagesPath = path.join(repoPath, 'packages/website/src/pages/kit');
+  const kitPagesPath = path.join(repoPath, 'packages/website/src/pages/kits');
 
   try {
     const handle = await fs.opendir(kitPagesPath);
