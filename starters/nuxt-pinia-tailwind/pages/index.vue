cea09caa2dbbf2c8fd7bb71c4d33fc3e98a8347e
jesus4497 <jesuspadron4497@gmail.com>
2022-05-27 12:50:07 -0400

    style: more improvements to homepage styles

--- starters/nuxt-pinia-tailwind/pages/index.vue
+++ starters/nuxt-pinia-tailwind/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center mt-5">
+  <div class="flex flex-col justify-center items-center mt-5 gap-2">
     <header class="bg-blue-600 w-3/5 p-4 text-center rounded mb-5">
       <h1 class="text-2xl font-bold text-white">
         Nuxt, Pinia and TailwindCSS Starter kit
