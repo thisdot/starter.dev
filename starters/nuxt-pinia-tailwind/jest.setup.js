c7fbbe270f1bd16ba9ee00d742aa9988c5208024
Jesus Padron <jesuspadron4497@gmail.com>
2022-06-03 14:36:31 -0400

    [Nuxt + Pinia + Tailwind]: New  API fetch component (#170)
    * feat: update readme file
    
    * feat: add counter component
    
    * feat: add counter component
    
    * feat: new counter component
    
    * fix: pinia warning
    
    * fix: delete wrong lock file
    
    * feat: add new fetch component
    
    * fix: using axios from context
    
    * fix: missing dependency

--- starters/nuxt-pinia-tailwind/jest.setup.js
+++ starters/nuxt-pinia-tailwind/jest.setup.js
@@ -1,1 +1,5 @@
-import '@testing-library/jest-dom';
+// Used for __tests__/testing-library.js
+// allows you to do things like:
+// expect(element).toHaveTextContent('hi!')
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
---
+++ starters/nuxt-pinia-tailwind/test/utils.ts
@@ -0,0 +1,5 @@
+/**
+ * Choosing to export testing library through a utility file instead to avoid major refactorings and allowing custom configuration.
+ * Please see: https://testing-library.com/docs/react-testing-library/setup as reference
+ */
+export * from '@testing-library/vue'
