5dfa7bbd314e6c1a872487cc6a228afa14d18914
Jerry Hogan <jerry.hogan@thisdot.co>
2022-06-20 13:43:19 +0100

    fix: storybook now works

--- starters/vue3-apollo-quasar/.storybook/main.js
+++ starters/vue3-apollo-quasar/.storybook/main.js
@@ -12,12 +12,15 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     // register webpack path aliases
     config.resolve.alias['~storybook'] = path.resolve(__dirname);
-    config.resolve.alias['@'] = path.resolve(__dirname, '..', 'src');
+    config.resolve.alias['src'] = path.resolve(__dirname, '..', 'src');
     // enable sass
     config.module.rules.push({
       test: /\.sass$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
     });
     return config;
+  },
+  core: {
+    builder: 'webpack5',
   },
 };
