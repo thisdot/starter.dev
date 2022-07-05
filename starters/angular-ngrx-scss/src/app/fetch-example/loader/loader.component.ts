dce9343570f685c12e01947840c73f39dbe9ef6d
Daian Scuarissi <scuarissid@gmail.com>
2022-07-05 18:17:19 +0200

    [angular-ngrx-scss] - Update loader styles on fetch example (#250)

---
+++ starters/angular-ngrx-scss/src/app/fetch-example/loader/loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent {}
