2c20d5dfa7b24462fb72a4a94117213ee2dbb2b0
Adrivinci <sheerikie@gmail.com>
2022-07-13 16:03:08 +0300

    new dots loader

--- starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
+++ starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 
 export const Loader = () => {
+  let circleCommonClasses = 'h-2.5 w-2.5 bg-current rounded-full';
+
   return (
-    <div className="w-60 h-24 border-2 rounded-md mx-auto mt-20">
-      <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-        <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-        <div className="flex flex-col space-y-3">
-          <div className="w-36 bg-gray-300 h-6 rounded-md "></div>
-          <div className="w-24 bg-gray-300 h-6 rounded-md "></div>
-        </div>
+    <div className="flex w-60 h-24  rounded-md mx-auto mt-auto">
+      <div className="flex animate-pulse items-center h-full justify-center space-x-5">
+        <div className="w-10 bg-red-200 h-6 rounded-md"></div>
+        <div className="w-10 bg-red-200 h-6 rounded-md"></div>
+        <div className="w-10 bg-red-200 h-6 rounded-md "></div>
+        <div className="w-10 bg-red-200 h-6 rounded-md "></div>
       </div>
     </div>
   );
