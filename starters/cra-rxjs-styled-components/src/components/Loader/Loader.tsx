4b8e7dc1b151a9b8abbebe3af15d79e94044b465
Adrivinci <sheerikie@gmail.com>
2022-07-13 23:29:43 +0300

    newer design

--- starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
+++ starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
@@ -6,10 +6,7 @@ export const Loader = () => {
   return (
     <div className="flex w-60 h-24  rounded-md mx-auto mt-auto">
       <div className="flex animate-pulse items-center h-full justify-center space-x-5">
-        <div className="w-10 bg-red-200 h-6 rounded-md"></div>
-        <div className="w-10 bg-red-200 h-6 rounded-md"></div>
-        <div className="w-10 bg-red-200 h-6 rounded-md "></div>
-        <div className="w-10 bg-red-200 h-6 rounded-md "></div>
+        <div className="w-60 bg-gray-200 h-6 rounded-md"></div>
       </div>
     </div>
   );
