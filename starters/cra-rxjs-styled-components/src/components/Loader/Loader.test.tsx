895ea48b5111efbc1c13509780a4171bf092032d
Adrivinci <sheerikie@gmail.com>
2022-07-14 22:31:29 +0300

    corrections on previous commit

--- starters/cra-rxjs-styled-components/src/components/Loader/Loader.test.tsx
+++ starters/cra-rxjs-styled-components/src/components/Loader/Loader.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Loader } from './Loader';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Add Loader', () => {
   it('should appear while waiting for response', () => {
-    const { container } = render(<Loader />, { wrapper: MemoryRouter });
-    expect(container.getElementsByClassName('w-60').length).toBe(1);
+    const { getByTestId } = render(<Loader />, { wrapper: MemoryRouter });
+    expect(getByTestId('loader')).toBeVisible();
   });
 });
--- starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
+++ starters/cra-rxjs-styled-components/src/components/Loader/Loader.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 export const Loader = () => {
-  let circleCommonClasses = 'h-2.5 w-2.5 bg-current rounded-full';
-
   return (
-    <div className="flex w-60 h-24  rounded-md mx-auto mt-auto">
+    <div
+      data-testid="loader"
+      className="flex w-60 h-24  rounded-md mx-auto mt-auto"
+    >
       <div className="flex animate-pulse items-center h-full justify-center space-x-5">
         <div className="w-60 bg-gray-200 h-6 rounded-md"></div>
       </div>
