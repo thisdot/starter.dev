86b102c7da9204ad4a20a252ffe523112586bc4b
jesus padron <jesuspadron4497@gmail.com>
2022-10-07 19:14:57 -0400

    chore: rename nuxt kit to nuxt 2

--- starters/nuxt2-pinia-tailwind/nuxt.config.js
+++ starters/nuxt2-pinia-tailwind/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-pinia-tailwind starter kit',
+    title: 'nuxt2-pinia-tailwind starter kit',
     htmlAttrs: {
       lang: 'en',
     },
