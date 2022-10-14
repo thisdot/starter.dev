4fe51ee5ce4d39857708f4f5c3896a6ad2e389d0
Jan Kaiser <honza.kaiser@gmail.com>
2022-10-14 23:17:12 +0200

    feat: made storybook render

---
+++ starters/qwik-fetch-tailwind/.storybook/preview.ts
@@ -0,0 +1,14 @@
+// Based on https://github.com/BuilderIO/qwik/discussions/787#discussioncomment-3715103
+import { JSXNode } from '@builder.io/qwik';
+import { QWIK_LOADER } from '@builder.io/qwik/loader/index';
+import { render } from '@builder.io/qwik';
+eval(QWIK_LOADER);
+
+export const decorators = [
+  (Story: () => JSXNode) => {
+    const parent = document.createElement('div');
+    const jsxNode = Story();
+    render(parent, jsxNode);
+    return parent;
+  },
+];
--- starters/qwik-fetch-tailwind/src/routes/counter/index.tsx
+++ starters/qwik-fetch-tailwind/src/routes/counter/index.tsx
@@ -1,23 +1,6 @@
-import { component$, useStore } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik';
+import { Counter } from './counter';
 
 export default component$(() => {
-  const store = useStore({
-    count: 0,
-  });
-
-  return (
-    <div>
-      <h1>Increment, Decrement and Reset Button Examples</h1>
-      <Display store={store} />
-      <button onClick$={() => store.count++}>Increment</button>
-      <button onClick$={() => store.count--}>Decrement</button>
-      <button onClick$={() => (store.count = 0)}>Reset</button>
-      <Link href="/">Return Home</Link>
-    </div>
-  );
-});
-
-export const Display = component$((props: { store: { count: number } }) => {
-  return <div>Count: {props.store.count}</div>;
+  return <Counter />;
 });
