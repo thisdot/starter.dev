9ad6c4e36f96fdcf6cbebf1dbb67dfc0c367076b
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-10-18 13:11:22 +0300

    [svelte kit starter] Add scss support (#390)

--- starters/svelte-kit-scss/src/routes/+page.svelte
+++ starters/svelte-kit-scss/src/routes/+page.svelte
@@ -23,7 +23,7 @@
 	<Counter />
 </section>
 
-<style>
+<style lang="scss">
 	section {
 		display: flex;
 		flex-direction: column;
@@ -42,13 +42,13 @@
 		width: 100%;
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		img {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			display: block;
+		}
 	}
 </style>
