af440b3fe0ae611b007303873bb412d45b379bb2
Jan Kaiser <honza.kaiser@gmail.com>
2022-10-19 12:01:15 +0200

    fix: removed "return home" link

--- starters/qwik-fetch-tailwind/src/routes/counter/index.tsx
+++ starters/qwik-fetch-tailwind/src/routes/counter/index.tsx
@@ -1,12 +1,10 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
 import { Counter } from './counter';
 
 export default component$(() => {
   return (
     <div>
       <Counter />
-      <Link href="/">Return Home</Link>
     </div>
   );
 });
