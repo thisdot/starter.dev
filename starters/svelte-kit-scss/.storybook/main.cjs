dc3e1d9ce051539cc87e76cef5ba480597795ffe
Ian Mungai <iansam74@gmail.com>
2022-10-19 11:00:21 +0300

    chore: setup storybook

---
+++ starters/svelte-kit-scss/.storybook/main.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/svelte",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "svelteOptions": {
+    "preprocess": import("../svelte.config.js").preprocess
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+}
