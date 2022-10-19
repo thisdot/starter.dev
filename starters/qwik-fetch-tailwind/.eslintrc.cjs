94d8d9ab23aa754a3e26efbe3d9f325dc0917e3a
Daian Scuarissi <scuarissid@gmail.com>
2022-10-19 11:04:55 +0200

    (feat): Add linting prettier to qwik pod #348 (#401)

--- starters/qwik-fetch-tailwind/.eslintrc.cjs
+++ starters/qwik-fetch-tailwind/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:qwik/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
