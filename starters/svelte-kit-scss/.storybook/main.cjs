b2d52d5a70abc5a21fba3aa6ebee252d1ea8e4d4
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-10-19 14:10:21 +0300

    [sveltekit-pod] 2. Setup Storybook (#404)
    * chore: setup storybook
    
    * chore: remove default stories folder
    
    * chore: add test counter component storybook
    
    * fix: storybook typescript compatiility
    
    * fix: remove commment

---
+++ starters/svelte-kit-scss/.storybook/main.cjs
@@ -0,0 +1,24 @@
+const { typescript: preprocessTs } = require('svelte-preprocess');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf"
+  ],
+  "framework": "@storybook/svelte",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "svelteOptions": {
+    "preprocess": [preprocessTs()]
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+}
