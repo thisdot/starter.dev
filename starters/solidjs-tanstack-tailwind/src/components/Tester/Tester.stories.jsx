3871716b6bddba774a132a20cdf3723995ebb52d
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-19 17:03:19 +0100

    chore: storybook added

---
+++ starters/solidjs-tanstack-tailwind/src/components/Tester/Tester.stories.jsx
@@ -0,0 +1,10 @@
+import Tester from '.';
+
+export default {
+  title: 'Example/ Tester',
+  argTypes: {},
+};
+
+const Template = (args) => <Tester {...args} />;
+
+export const Default = Template.bind({});
