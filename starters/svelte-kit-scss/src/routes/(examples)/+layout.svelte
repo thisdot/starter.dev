341645c475acd79590535fba41c44734b6a07ff7
Ian Mungai <iansam74@gmail.com>
2022-10-19 17:35:04 +0300

    chore: add design fetch component

--- starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
+++ starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
@@ -4,3 +4,19 @@
 		<a href="/">Return Home</a>
 	</div>
 </div>
+
+<style lang="scss">
+	div {
+        text-align: center;
+		margin: 10px 0;
+		
+		a {
+			font-size: 1.2rem;
+			color: rgb(37 99 235);
+			&:hover {
+				color: rgb(30 64 175);
+				text-decoration: underline;
+			}
+		}
+	}
+</style>
