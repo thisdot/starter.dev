643b547b01e6bef675a69498dece7ec7fcb676ce
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-19 17:10:36 +0100

    Solidjs/add routing homepage (#407)
    * in progress
    
    * need to merge tailwind into this
    
    * chore: routing completed
    
    * updates
    
    * fixed comments, changed route package
    
    * fix comments

--- starters/solidjs-tanstack-tailwind/src/index.jsx
+++ starters/solidjs-tanstack-tailwind/src/index.jsx
@@ -1,7 +1,14 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
+import { Router } from '@solidjs/router';
 import './index.css';
 import App from './App';
 
-render(() => <App />, document.getElementById('root'));
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById('root')
+);
---
+++ starters/solidjs-tanstack-tailwind/src/pages/Counter/index.jsx
@@ -0,0 +1,9 @@
+const Counter = () => {
+  return (
+    <div>
+      <h2>Counter</h2>
+    </div>
+  );
+};
+
+export default Counter;
