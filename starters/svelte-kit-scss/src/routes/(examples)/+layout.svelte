0c117d82add2cd71af14459e62c1942e7886a701
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-10-20 16:53:02 +0300

    Formatting adjustments (#416)

--- starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
+++ starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
@@ -1,6 +1,6 @@
 <div>
-	<slot />
-	<div>
-		<a href="/">Return Home</a>
-	</div>
+  <slot />
+  <div>
+    <a href="/">Return Home</a>
+  </div>
 </div>
--- starters/svelte-kit-scss/src/routes/(examples)/api-example/+page.svelte
+++ starters/svelte-kit-scss/src/routes/(examples)/api-example/+page.svelte
@@ -1,4 +1,4 @@
 <div>
-	<h1>Fetch Data From API</h1>
-	<div>Fetch component works</div>
+  <h1>Fetch Data From API</h1>
+  <div>Fetch component works</div>
 </div>
