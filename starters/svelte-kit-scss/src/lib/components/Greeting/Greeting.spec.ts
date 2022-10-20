8bb7362afcde0c43002f0e92e581c312a746fc17
Ian Mungai <iansam74@gmail.com>
2022-10-21 00:36:08 +0300

    chore: setup greeting test

--- starters/svelte-kit-scss/src/lib/components/Greeting/Greeting.spec.ts
+++ starters/svelte-kit-scss/src/lib/components/Greeting/Greeting.spec.ts
@@ -1,17 +1,11 @@
 import Greeting from './Greeting.svelte';
+import { render } from '@testing-library/svelte';
 
 describe('Greeting Component', () => {
-  test('should render the component', () => {
-    // Create a new container for the test
-    const host = document.createElement('div');
+  test('should show title', () => {
+    const message = 'from Vitest'
+    const { getByText } = render(Greeting, { message });
 
-    // Append the new container in the HTML body
-    document.body.appendChild(host);
-
-    // Create an instance of the vertical tab
-    const instance = new Greeting({ target: host });
-
-    // Check if the instance created
-    expect(instance).toBeTruthy();
-  });
+    expect(() => getByText(`Hello, ${message}`))
+  })
 });
