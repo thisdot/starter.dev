648ce1184f5bd01f21ac44c1d728769afbae775e
Ian Mungai <iansam74@gmail.com>
2022-10-20 13:52:38 +0300

    Merge: Merger with integration-branch-sveltepod-kit

--- starters/svelte-kit-scss/.storybook/main.cjs
+++ starters/svelte-kit-scss/.storybook/main.cjs
@@ -1,31 +1,28 @@
 const { 
-  typescript: preprocessTs, 
-  scss: preprocessScss 
-} = require('svelte-preprocess');
+	typescript: preprocessTs, 
+	scss: preprocessScss 
+  } = require('svelte-preprocess');
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-svelte-csf",
-    "@storybook/preset-scss"
-  ],
-  "framework": "@storybook/svelte",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "svelteOptions": {
-    "preprocess": [
-      preprocessTs(), // sveltekit-storybook typescript support
-      preprocessScss() // sveltekit-storybook sass support
-    ]
-  },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf',
+		'@storybook/preset-scss'
+	],
+	framework: '@storybook/svelte',
+	core: {
+		builder: '@storybook/builder-vite'
+	},
+	svelteOptions: {
+		"preprocess": [
+		  preprocessTs(), // sveltekit-storybook typescript support
+		  preprocessScss() // sveltekit-storybook sass support
+		]
+	},
+	features: {
+		storyStoreV7: true
+	}
+};
---
+++ starters/svelte-kit-scss/src/lib/components/Counter.spec.ts
@@ -0,0 +1,17 @@
+import Counter from './Counter.svelte';
+
+describe('Counter Component', () => {
+	test('should render the component', () => {
+		// Create a new container for the test
+		const host = document.createElement('div');
+
+		// Append the new container in the HTML body
+		document.body.appendChild(host);
+
+		// Create an instance of the vertical tab
+		const instance = new Counter({ target: host });
+
+		// Check if the instance created
+		expect(instance).toBeTruthy();
+	});
+});
