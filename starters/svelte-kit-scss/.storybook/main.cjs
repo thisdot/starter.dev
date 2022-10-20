73197338b45d5330d6402921aba58ad5f3c838b5
Ian Mungai <iansam74@gmail.com>
2022-10-20 12:29:41 +0300

    fix: svelte sass support

--- starters/svelte-kit-scss/.storybook/main.cjs
+++ starters/svelte-kit-scss/.storybook/main.cjs
@@ -1,4 +1,7 @@
-const { typescript: preprocessTs } = require('svelte-preprocess');
+const { 
+	typescript: preprocessTs, 
+	scss: preprocessScss 
+  } = require('svelte-preprocess');
 
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
@@ -6,14 +9,18 @@ module.exports = {
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
-		'@storybook/addon-svelte-csf'
+		'@storybook/addon-svelte-csf',
+		'@storybook/preset-scss'
 	],
 	framework: '@storybook/svelte',
 	core: {
 		builder: '@storybook/builder-vite'
 	},
 	svelteOptions: {
-		preprocess: [preprocessTs()]
+		"preprocess": [
+		  preprocessTs(), // sveltekit-storybook typescript support
+		  preprocessScss() // sveltekit-storybook sass support
+		]
 	},
 	features: {
 		storyStoreV7: true
