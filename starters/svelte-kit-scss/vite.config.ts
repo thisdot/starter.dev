eb4800decae9de36e904b76d99093324c2aeb63b
Ian Mungai <iansam74@gmail.com>
2022-10-20 18:44:03 +0300

    fix: viteconfig.js

--- starters/svelte-kit-scss/vite.config.ts
+++ starters/svelte-kit-scss/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vitest/config';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [svelte({ hot: !process.env.VITEST })],
-	test: {
-		globals: true,
-		environment: 'jsdom'
-	}
-});
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
+
+export default config;
