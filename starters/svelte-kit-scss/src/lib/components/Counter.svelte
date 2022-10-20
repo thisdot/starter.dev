e31e9a383d14aa43bfe0f5cad33d548e1d330f49
Ian Mungai <iansam74@gmail.com>
2022-10-20 12:34:07 +0300

    test: with counter sass

--- starters/svelte-kit-scss/src/lib/components/Counter.svelte
+++ starters/svelte-kit-scss/src/lib/components/Counter.svelte
@@ -34,30 +34,29 @@
 	</button>
 </div>
 
-<style>
+<style lang="scss">
 	.counter {
 		display: flex;
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
-	}
 
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		touch-action: manipulation;
-		font-size: 2rem;
-	}
+		button {
+			width: 2em;
+			padding: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border: 0;
+			background-color: transparent;
+			touch-action: manipulation;
+			font-size: 2rem;
+		}
 
-	.counter button:hover {
-		background-color: var(--color-bg-1);
+		button:hover {
+			background-color: var(--color-bg-1);
+		}
 	}
-
 	svg {
 		width: 25%;
 		height: 25%;
