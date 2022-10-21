956115285ab40e3af211f9b3567f0299a4eac5d8
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2022-10-21 21:17:34 +0300

    tests

--- starters/svelte-kit-scss/src/lib/components/Counter/Counter.spec.ts
+++ starters/svelte-kit-scss/src/lib/components/Counter/Counter.spec.ts
@@ -1,17 +1,52 @@
 import Counter from './Counter.svelte';
+import { cleanup, fireEvent, render } from '@testing-library/svelte';
 
 describe('Counter Component', () => {
-  test('should render the component', () => {
-    // Create a new container for the test
-    const host = document.createElement('div');
+  afterEach(() => {
+    cleanup();
+  });
 
-    // Append the new container in the HTML body
-    document.body.appendChild(host);
+  it('should render the component', () => {
+    const { component } = render(Counter);
+    expect(component).toBeTruthy();
+  });
 
-    // Create an instance of the vertical tab
-    const instance = new Counter({ target: host });
+  it('should have correct initial value', () => {
+    const { getByText } = render(Counter);
+    const span = getByText(/Count:/);
+    expect(span.innerHTML).toBe('Count: 0');
+  });
 
-    // Check if the instance created
-    expect(instance).toBeTruthy();
+  it('should increase the counter value', async () => {
+    const { getByText } = render(Counter);
+    const span = getByText(/Count:/);
+    const button = getByText(/Increment/);
+    expect(span.innerHTML).toBe('Count: 0');
+    await fireEvent.click(button);
+    expect(span.innerHTML).toBe('Count: 1');
+    await fireEvent.click(button);
+    expect(span.innerHTML).toBe('Count: 2');
+  });
+
+  it('should reset the counter value', async () => {
+    const { getByText } = render(Counter);
+    const span = getByText(/Count:/);
+    const incrementButton = getByText(/Increment/);
+    const resetButton = getByText(/Reset/);
+    await fireEvent.click(incrementButton);
+    await fireEvent.click(resetButton);
+    expect(span.innerHTML).toBe('Count: 0');
+  });
+
+  it('should decrease the counter value', async () => {
+    const { getByText, component } = render(Counter);
+    console.log(component);
+    const span = getByText(/Count:/);
+    const button = getByText(/Decrement/);
+    expect(span.innerHTML).toBe('Count: 0');
+    await fireEvent.click(button);
+    expect(span.innerHTML).toBe('Count: -1');
+    await fireEvent.click(button);
+    expect(span.innerHTML).toBe('Count: -2');
   });
 });
