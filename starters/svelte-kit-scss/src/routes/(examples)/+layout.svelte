d92240afbad517efcef18f7feeb6e0c8d009ac9b
Ian Mungai <iansam74@gmail.com>
2022-10-21 14:29:00 +0300

    fix: example layout

--- starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
+++ starters/svelte-kit-scss/src/routes/(examples)/+layout.svelte
@@ -1,6 +1,6 @@
 <div class="example-container">
   <slot />
-  <div>
+  <div class="footer">
     <a href="/">Return Home</a>
   </div>
 </div>
@@ -8,8 +8,10 @@
 <style lang="scss">
   .example-container {
     text-align: center;
+    width: 60%;
+    margin: 1.25rem auto;
 
-    div{
+    .footer{
       margin: 0.625rem 0;
       a {
         font-size: 1.2rem;
