855c38825f35fdd5d0e6ad0e0922b55547ceaf54
Jerry Hogan <jerry.hogan@thisdot.co>
2022-10-25 10:30:47 +0100

    chore: loader skeleton

--- starters/solidjs-tailwind/src/components/FetchExample/Greeting.jsx
+++ starters/solidjs-tailwind/src/components/FetchExample/Greeting.jsx
@@ -6,13 +6,15 @@ const Greeting = () => {
   return (
     <Switch>
       <Match when={message.error}>
-        <p class="text-red-500">There was an error loading your greeting :(</p>
+        <p class="text-red-500">
+          There was an error loading your greeting :(
+        </p>
       </Match>
       <Match when={!message.loading}>
         <p>Message: {message()}</p>
       </Match>
       <Match when={message.loading}>
-        <p>Get ready to be greeted!</p>
+        <div class="w-[80%] lg:w-[20%] mx-auto grow animate-pulse bg-gray-200 rounded-md p-3" />
       </Match>
     </Switch>
   );
