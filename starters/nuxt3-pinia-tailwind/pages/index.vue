347e408aac65ea711c933848c4489890c5dafd5b
temitope adesoji <temitopeadesoji23@gmail.com>
2022-11-03 11:37:05 +0100

    Create project scaffolding
    - install Nuxt3 with TypeScript
    - Install and Setup Tailwind
    - Install and Setup Pinia

---
+++ starters/nuxt3-pinia-tailwind/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col justify-center items-center mt-5 gap-2">
+      <NuxtLink
+        to="/counter"
+        class="text-lg text-blue-600 underline hover:text-blue-400"
+      >
+        See Counter example component
+      </NuxtLink>
+  
+      <NuxtLink
+        to="/api-example"
+        class="text-lg text-blue-600 underline hover:text-blue-400"
+      >
+        See Fetch API example component
+      </NuxtLink>
+    </div>
+</template>
+  
