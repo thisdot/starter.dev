6701f052ef858ebd8715f51345200918909e914f
temitope adesoji <temitopeadesoji23@gmail.com>
2022-11-03 11:37:05 +0100

    Create project scaffolding
    - install Nuxt3 with TypeScript
    - Install and Setup Tailwind
    - Install and Setup Pinia

---
+++ starters/nuxt3-pinia-tailwind/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col justify-center items-center mt-5 gap-2">
+    <header class="bg-blue-600 w-3/5 p-4 text-center rounded mb-5">
+      <h1 class="text-2xl font-bold text-white">
+        Nuxt 3, Pinia and TailwindCSS Starter kit
+      </h1>
+    </header>
+
+    <NuxtPage />
+  </div>
+</template>
