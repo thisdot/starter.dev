cb52a7f668e9af9bd7d2618d96f145821e7b6cca
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-11-04 08:35:21 +0200

    Setup absolute imports

--- starters/expo-zustand-styled-component/App.tsx
+++ starters/expo-zustand-styled-component/App.tsx
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button />
+      <Button title="+" onPress={() => null} />
       <StatusBar style="auto" />
     </View>
   );
