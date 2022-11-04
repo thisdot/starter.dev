37e4c5be9adc6146f8f0c18c8bc196e1bc43ab16
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-11-04 08:38:51 +0200

    Update border color

--- starters/expo-zustand-styled-component/src/components/Button/Button.styles.ts
+++ starters/expo-zustand-styled-component/src/components/Button/Button.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const TouchableOpacityStyled = styled.TouchableOpacity`
   width: 70px;
   height: 70px;
-  border: 1px solid black;
+  border: 1px solid rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
