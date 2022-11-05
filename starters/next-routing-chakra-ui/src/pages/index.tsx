9fd53edf95b727f4953d9f2940e181e1b2399e8c
jdwilkin4 <jwilkin4@hotmail.com>
2022-11-04 17:53:10 -0700

    style: homepage

--- starters/next-routing-chakra-ui/src/pages/index.tsx
+++ starters/next-routing-chakra-ui/src/pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box, Center, UnorderedList, ListItem, Flex } from '@chakra-ui/react';
+
+type Routes = {
+  text: string;
+  route: string;
+};
+
 const Home: NextPage = () => {
+  const routesArr: Routes[] = [
+    { text: 'See Counter example component', route: '/counter-example' },
+    { text: 'See Fetch example component', route: 'fetch-example' },
+  ];
+
   return (
     <div>
       <Head>
@@ -10,18 +21,24 @@ const Home: NextPage = () => {
         <meta name="description" content="This is a starter kit for NextJS 12 and Chakra UI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header className="">
-        <Heading bg="blue">Create Next app with NextJS 12, Chakra UI</Heading>
-        <ul>
-          <li className="">
-            <Link href="/counter-example">See Counter example component</Link>
-          </li>
-          <li className="">
-            <Link href="/fetch-example">See Fetch example component</Link>
-          </li>
-        </ul>
-      </header>
+      <Box>
+        <Center>
+          <Heading color="white" borderRadius={4} mt={8} p={4} bg="#2464ec">
+            NextJS 12 and Chakra UI Starter Kit
+          </Heading>
+        </Center>
+        <Center>
+          <UnorderedList mt={8} listStyleType="none">
+            <Flex flexDirection="column" justifyContent="center" alignItems="center">
+              {routesArr.map(({ route, text }) => (
+                <ListItem key={route} fontSize="2xl" textDecoration="underline" color="#376fec">
+                  <Link href={route}>{text}</Link>
+                </ListItem>
+              ))}
+            </Flex>
+          </UnorderedList>
+        </Center>
+      </Box>
     </div>
   );
 };
