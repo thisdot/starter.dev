e694b2a274801c1e71fca40fada52e9406d53bf3
jdwilkin4 <jwilkin4@hotmail.com>
2022-11-10 11:14:34 -0800

    refactoring to use getServerSideProps

--- starters/next-routing-chakra-ui/src/components/greeting/index.tsx
+++ starters/next-routing-chakra-ui/src/components/greeting/index.tsx
@@ -1,48 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Text, Center, Alert, AlertIcon, AlertTitle, AlertDescription, Skeleton } from '@chakra-ui/react';
+import { Text, Center } from '@chakra-ui/react';
 
-export const Greeting = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [greeting, setGreeting] = useState('');
-
-  useEffect(() => {
-    fetch(`https://api.starter.dev/hello?greeting=from This Dot Labs!`)
-      .then((res) => res.text())
-      .then((data) => {
-        setGreeting(data);
-      })
-      .catch((error) => {
-        setError(true);
-        console.error(`There was an error fetching your data: ${error}`);
-      })
-      .finally(() => setIsLoading(false));
-  }, []);
-
-  if (isLoading) {
-    return (
-      <Center my={4}>
-        <Text display="block" fontSize="2xl">
-          Loading data...
-          <Skeleton startColor="blue.500" endColor="blue.700" height="20px" />
-        </Text>
-      </Center>
-    );
-  }
-
-  if (error) {
-    return (
-      <Alert my={8} w="50%" mx="auto" status="error">
-        <AlertIcon />
-        <AlertTitle fontSize="xl">Error!</AlertTitle>
-        <AlertDescription fontSize="xl"> There was an issue with loading your data. Please refresh the page or try again later.</AlertDescription>
-      </Alert>
-    );
-  }
-
+export const Greeting = ({ name }: { name: string | string[] | undefined }) => {
   return (
     <Center my={4}>
-      <Text fontSize="xl">Message: {greeting}</Text>
+      <Text fontSize="xl">Message: {name}</Text>
     </Center>
   );
 };
---
+++ starters/next-routing-chakra-ui/src/pages/fetch-example/[[...slug]].tsx
@@ -0,0 +1,41 @@
+import type { GetServerSideProps, NextPage } from 'next';
+import Link from 'next/link';
+import { Greeting } from '../../components/greeting';
+import { Heading, Center } from '@chakra-ui/react';
+
+const checkSlugType = (slug: string[] | string | undefined) => {
+  if (slug === undefined) {
+    return 'This Dot Labs';
+  }
+  return slug === typeof 'string' ? slug : slug[slug.length - 1];
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { slug } = context.query;
+  console.log(context);
+  const res = await fetch(`https://api.starter.dev/hello?greeting=from ${checkSlugType(slug)}!`);
+  const userStr = await res.text();
+
+  return {
+    props: { userStr },
+  };
+};
+
+const FetchExample: NextPage<{ userStr: string }> = ({ userStr }) => {
+  return (
+    <div>
+      <Center>
+        <Heading pb={4} borderBottom="4px solid #2464ec" mt={8}>
+          NextJS 12 Fetching Data From API
+        </Heading>
+      </Center>
+
+      <Greeting name={userStr} />
+      <Center fontSize="xl" textDecoration="underline" color="#376fec">
+        <Link href="/">Return Home</Link>
+      </Center>
+    </div>
+  );
+};
+
+export default FetchExample;
