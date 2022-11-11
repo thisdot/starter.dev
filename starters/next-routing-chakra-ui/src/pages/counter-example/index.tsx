ed44dfe1ea5eef3fd48c0e0b3cb0807c7ea74c68
jdwilkin4 <jwilkin4@hotmail.com>
2022-11-11 14:01:47 -0800

    refactor: header styles and counter example

--- starters/next-routing-chakra-ui/src/pages/counter-example/index.tsx
+++ starters/next-routing-chakra-ui/src/pages/counter-example/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import { Center } from '@chakra-ui/react';
+import type { NextPage } from "next";
+import Link from "next/link";
+import { Center, Heading } from "@chakra-ui/react";
 
-const Counter: NextPage = () => {
+const CounterExample: NextPage = () => {
   return (
     <>
-      This is for the counter example
+      <Center>
+        <Heading as="h1" pb={4} borderBottom="4px solid #2464ec" mt={8}>
+          Increment, Decrement and Reset Button Example
+        </Heading>
+      </Center>
+
+      <div className="">{/*counter goes here*/}</div>
       <Center fontSize="xl" textDecoration="underline" color="#376fec">
         <Link href="/">Return Home</Link>
       </Center>
@@ -13,4 +19,4 @@ const Counter: NextPage = () => {
   );
 };
 
-export default Counter;
+export default CounterExample;
