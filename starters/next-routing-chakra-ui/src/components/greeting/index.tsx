84037c4963b894c2276e353d181122c979a21c56
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2022-11-10 16:18:08 -0800

    feat: adding fetch example (#488)
    * feat: adding fetch example
    
    * style: homepage
    
    * adding styles
    
    * refactoring to use getServerSideProps
    
    * fix: removing console statement
    
    * fix: routing

---
+++ starters/next-routing-chakra-ui/src/components/greeting/index.tsx
@@ -0,0 +1,9 @@
+import { Text, Center } from '@chakra-ui/react';
+
+export const Greeting = ({ name }: { name: string | string[] | undefined }) => {
+  return (
+    <Center my={4}>
+      <Text fontSize="xl">Message: {name}</Text>
+    </Center>
+  );
+};
--- starters/next-routing-chakra-ui/src/pages/_app.tsx
+++ starters/next-routing-chakra-ui/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
