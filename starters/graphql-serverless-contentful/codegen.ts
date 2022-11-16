b7a3d1a5b6f222e06b30c31cabb2f9c0089d990f
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-11-17 00:01:54 +0200

    fixed codegen

--- starters/graphql-serverless-contentful/codegen.ts
+++ starters/graphql-serverless-contentful/codegen.ts
@@ -1,17 +1,16 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:4000",
+  schema: './src/schema/index.ts',
   generates: {
-    "src/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-resolvers"]
+    'src/generated/graphql.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
 };
 
 export default config;
