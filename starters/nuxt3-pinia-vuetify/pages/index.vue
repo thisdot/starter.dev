7c3249a309eff70a8fbbd8b20b4e02979c9f1277
temitope adesoji <temitopeadesoji23@gmail.com>
2022-11-17 15:31:23 +0100

    fix linting

--- starters/nuxt3-pinia-vuetify/pages/index.vue
+++ starters/nuxt3-pinia-vuetify/pages/index.vue
@@ -1,17 +1,16 @@
 <template>
-	<div class="d-flex flex-column justify-center align-center mt-5">
-		<NuxtLink
-			to="/counter"
-			class="text-subtitle-1 text-blue-darken-2"
-		>
-			See Counter example component
-		</NuxtLink>
-		<NuxtLink
-			to="/api-example"
-			class="text-subtitle-1 text-blue-darken-2 mt-4"
-		>
-			See Fetch API example component
-		</NuxtLink>
-	</div>
+  <div class="d-flex flex-column justify-center align-center mt-5">
+    <NuxtLink
+      to="/counter"
+      class="text-subtitle-1 text-blue-darken-2"
+    >
+      See Counter example component
+    </NuxtLink>
+    <NuxtLink
+      to="/api-example"
+      class="text-subtitle-1 text-blue-darken-2 mt-4"
+    >
+      See Fetch API example component
+    </NuxtLink>
+  </div>
 </template>
-  
