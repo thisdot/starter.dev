34e250542fdd1a200e76ec863a8985873ec06c4b
Balázs Tápai <balazs.tapai1990@gmail.com>
2022-11-22 11:32:18 +0100

    feat: document database setup and seeding

--- starters/express-typescript-typeorm-postgres/src/datasource.ts
+++ starters/express-typescript-typeorm-postgres/src/datasource.ts
@@ -1,15 +1,17 @@
 import { DataSource } from 'typeorm';
 
-console.log(process.env.DATABASE_PASSWORD)
+const IS_LOGGING_ENABLED = process.env.ENABLE_LOGGING
+  ? process.env.ENABLE_LOGGING === 'true'
+  : true;
 
 export const dataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt( process.env.DATABASE_PORT || '5432'),
-  username: process.env.DATABASE_USER || 'postgres',
+  port: parseInt(process.env.DATABASE_PORT || '5432'),
+  username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: 'pgdatabase',
+  database: process.env.DATABASE_NAME,
   entities: ['**/*.entity.js'],
-  logging: true,
+  logging: IS_LOGGING_ENABLED,
   synchronize: true,
 });
