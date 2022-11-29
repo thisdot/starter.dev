9a0911bf22c85d856e628651ce65a7f96c901eaa
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-11-29 14:25:22 +0200

    Add the comment model

--- starters/graphql-serverless-contentful/src/schema/index.ts
+++ starters/graphql-serverless-contentful/src/schema/index.ts
@@ -1,8 +1,17 @@
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { helloTypeDefs, helloResolvers } from './hello';
 import { postTypeDefs, postResolvers } from './post';
+import { commentTypeDefs, commentResolvers } from './comment';
 
-export const typeDefs = mergeTypeDefs([helloTypeDefs, postTypeDefs]);
+export const typeDefs = mergeTypeDefs([
+  helloTypeDefs,
+  postTypeDefs,
+  commentTypeDefs,
+]);
 
-export const resolvers = mergeResolvers([helloResolvers, postResolvers]);
+export const resolvers = mergeResolvers([
+  helloResolvers,
+  postResolvers,
+  commentResolvers,
+]);
 
