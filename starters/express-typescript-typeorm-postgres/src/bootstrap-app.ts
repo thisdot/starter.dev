79f6838cac500967a971567009d533fffb2e35fc
Tápai Balázs <balazs@thisdot.co>
2022-11-30 15:59:38 +0100

    [NODE] feat: added swagger open api generator (#559)
    * feat: added swagger open api generator
    
    * fix: readme update
    
    * fix: remove swagger.json generation

--- starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
+++ starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
@@ -1,9 +1,15 @@
 import express, { Express } from 'express';
+import expressOasGenerator, { SPEC_OUTPUT_FILE_BEHAVIOR } from 'express-oas-generator';
 import { apiRouter } from './controllers/router';
 
 export function bootstrapApp(): Express {
   const app = express();
+  expressOasGenerator.handleResponses(app, {
+    specOutputFileBehavior: SPEC_OUTPUT_FILE_BEHAVIOR.RECREATE,
+    swaggerDocumentOptions: {},
+  });
   app.use(express.json());
   app.use('/api', apiRouter);
+  expressOasGenerator.handleRequests();
   return app;
 }
