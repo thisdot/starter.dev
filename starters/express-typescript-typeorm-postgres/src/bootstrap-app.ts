89857f56d8fce343dc27e67c03bb9ac7f00ebec6
Balázs Tápai <balazs.tapai1990@gmail.com>
2022-11-30 10:20:08 +0100

    feat: added swagger open api generator

--- starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
+++ starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
@@ -1,9 +1,16 @@
 import express, { Express } from 'express';
+import expressOasGenerator, { SPEC_OUTPUT_FILE_BEHAVIOR } from 'express-oas-generator';
 import { apiRouter } from './controllers/router';
 
 export function bootstrapApp(): Express {
   const app = express();
+  expressOasGenerator.handleResponses(app, {
+    specOutputPath: './swagger.json',
+    specOutputFileBehavior: SPEC_OUTPUT_FILE_BEHAVIOR.RECREATE,
+    swaggerDocumentOptions: {},
+  });
   app.use(express.json());
   app.use('/api', apiRouter);
+  expressOasGenerator.handleRequests();
   return app;
 }
