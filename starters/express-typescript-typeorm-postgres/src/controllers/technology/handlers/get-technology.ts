30d779eb1ea61d534d2fd9e950681d5f9495dec7
Tápai Balázs <balazs@thisdot.co>
2022-12-01 14:56:17 +0100

    [NODE] feat: added error handlers (#569)
    * feat: added error handlers
    
    * fix: return with 404 on not found technologies

--- starters/express-typescript-typeorm-postgres/src/controllers/technology/handlers/get-technology.ts
+++ starters/express-typescript-typeorm-postgres/src/controllers/technology/handlers/get-technology.ts
@@ -1,13 +1,50 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import { Result } from '../../../constants/result';
 import { dataSource } from '../../../datasource';
 import { Technology } from '../../../entities/technology.entity';
+import { ErrorResult, SuccessResult } from '../../../interfaces/results';
 
-export async function getTechnology(req: Request, res: Response): Promise<void> {
-  const technologyId: number = parseInt(req.params.technologyId);
-  const technology: Technology = await dataSource.getRepository(Technology).findOne({
-    where: {
-      id: technologyId,
-    },
-  });
-  res.json(technology);
+export async function getTechnology(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const technologyId: number = parseInt(req.params.technologyId);
+    const technologyResult = await findTechnology(technologyId);
+
+    if (technologyResult.type === Result.ERROR) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        error: getReasonPhrase(StatusCodes.NOT_FOUND),
+        details: technologyResult.message,
+      });
+      return;
+    }
+    res.json(technologyResult.data);
+  } catch (e) {
+    next(e);
+  }
+}
+
+function findTechnology(technologyId: number): Promise<SuccessResult<Technology> | ErrorResult> {
+  return dataSource
+    .getRepository(Technology)
+    .findOne({
+      where: {
+        id: technologyId,
+      },
+    })
+    .then((result) => {
+      if (!result) {
+        return {
+          type: Result.ERROR,
+          message: `Could not find technology with id: ${technologyId}`,
+        };
+      }
+      return {
+        type: Result.SUCCESS,
+        data: result,
+      };
+    });
 }
