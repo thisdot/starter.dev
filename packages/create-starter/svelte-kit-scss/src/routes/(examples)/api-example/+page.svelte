24bdf0455961128a869c9ab7c8f57a265f634a91
Ian Mungai <iansam74@gmail.com>
2022-12-01 09:22:42 +0300

    remove: unused folder

--- packages/create-starter/svelte-kit-scss/src/routes/(examples)/api-example/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Greeting from '$lib/components/Greeting/Greeting.svelte';
-
-  export let data: {
-    message: string;
-  };
-</script>
-
-<Greeting message={data.message} />
--- packages/create-starter/svelte-kit-scss/src/routes/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import './styles.scss';
-</script>
-
-<slot />
