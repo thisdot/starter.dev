10e80fcdf81088cb4e6eeb2ba22cd8313df03bb8
Ian Mungai <iansam74@gmail.com>
2022-12-01 16:16:30 +0300

    fix: allowedOrigins if empty array

--- starters/express-typescript-typeorm-postgres/src/middlewares/cors/index.ts
+++ starters/express-typescript-typeorm-postgres/src/middlewares/cors/index.ts
@@ -6,19 +6,21 @@ let middleware = cors();
 if (process.env.CORS_ALLOWED_ORIGINS) {
   try {
     const parsedOriginArray = JSON.parse(process.env.CORS_ALLOWED_ORIGINS);
-    const allowedOrigins: string[] = Array.isArray(parsedOriginArray) ? parsedOriginArray : [];
+    if (Array.isArray(parsedOriginArray)) {
+      const allowedOrigins: string[] = parsedOriginArray;
 
-    const corsOptions = {
-      origin: (origin, callback) => {
-        if (allowedOrigins.some((allowedOrigin) => allowedOrigin.startsWith(origin))) {
-          callback(null, true);
-        } else {
-          callback(new Error('The Current Origin is not allowed by CORS'));
-        }
-      },
-      optionsSuccessStatus: 200,
-    };
-    middleware = cors(corsOptions);
+      const corsOptions = {
+        origin: (origin, callback) => {
+          if (allowedOrigins.some((allowedOrigin) => allowedOrigin.startsWith(origin))) {
+            callback(null, true);
+          } else {
+            callback(new Error('The Current Origin is not allowed by CORS'));
+          }
+        },
+        optionsSuccessStatus: 200,
+      };
+      middleware = cors(corsOptions);
+    }
   } catch (e) {
     console.error(`An error occurred during CORS rule setup, defaulted to ALLOW-ALL`, e);
   }
