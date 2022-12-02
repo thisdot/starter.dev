a6f9165222d377340a4c56d5e629bdd5f18c815b
jdwilkin4 <jwilkin4@hotmail.com>
2022-12-02 09:36:06 -0800

    chore: adding serverless icons and updating config file

--- packages/website/src/config.tsx
+++ packages/website/src/config.tsx
@@ -36,6 +36,7 @@ import {
   PiniaIcon,
   QwikIcon,
   SolidJsIcon,
+  ServerlessIcon,
 } from './icons';
 
 export interface NavItem {
@@ -99,7 +100,7 @@ export const TECHNOLOGIES = [
     tags: ['Core UI'],
     Icon: (props) => <VueIcon {...props} />,
   },
-   {
+  {
     key: 'solidjs',
     name: 'SolidJs',
     tags: ['Core UI'],
@@ -263,8 +264,14 @@ export const TECHNOLOGIES = [
   },
   {
     key: 'apollo',
-    name: 'Apollo',
-    tags: ['Data Management'],
+    name: 'Apollo Client',
+    tags: ['API Specification'],
+    Icon: (props) => <ApolloIcon {...props} />,
+  },
+  {
+    key: 'apollo-server',
+    name: 'Apollo Server',
+    tags: ['Middleware'],
     Icon: (props) => <ApolloIcon {...props} />,
   },
   {
@@ -284,6 +291,12 @@ export const TECHNOLOGIES = [
     name: 'Pinia',
     tags: ['Data Management'],
     Icon: (props) => <PiniaIcon {...props} />,
+  },
+  {
+    key: 'serverless',
+    name: 'Serverless',
+    tags: ['Infrastructure'],
+    Icon: (props) => <ServerlessIcon {...props} />,
   },
 ];
 
