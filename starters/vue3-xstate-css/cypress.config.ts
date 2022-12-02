b401b3f06f8cfd0eb4f90384fce1a292d2155b35
LindaT <linda@thisdot.co>
2022-12-01 18:46:45 -0600

    feat: setup lint and format rules, run both

--- starters/vue3-xstate-css/cypress.config.ts
+++ starters/vue3-xstate-css/cypress.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:4173',
   },
   component: {
-    specPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
     devServer: {
-      framework: "vue",
-      bundler: "vite",
+      framework: 'vue',
+      bundler: 'vite',
     },
   },
 });
