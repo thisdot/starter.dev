449e35bbe90ec6053dda6b18d9fdce34dba2ff7e
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-12-02 15:42:52 +0200

    Add seeding script

---
+++ starters/graphql-serverless-contentful/scripts/seed.ts
@@ -0,0 +1,31 @@
+// load dotenv config
+require('dotenv').config();
+
+import { CommentModel } from '../src/models/CommentModel';
+import { PostModel } from '../src/models/PostModel';
+
+// Add some dummy data to the database
+
+console.log('Seeding database...');
+
+PostModel.create('Hello world!');
+PostModel.create('Good morning!');
+PostModel.create('Good afternoon!');
+PostModel.create('Good evening!');
+PostModel.create('Have a nice day!');
+PostModel.create('Have a nice weekend!');
+
+CommentModel.create('How are you?');
+CommentModel.create('I am fine, thank you!');
+CommentModel.create('How is the weather?');
+CommentModel.create('It is sunny today!');
+CommentModel.create('What are you doing?');
+CommentModel.create('I am coding!');
+CommentModel.create('What is your favorite color?');
+CommentModel.create('My favorite color is blue!');
+CommentModel.create('What is your favorite food?');
+CommentModel.create('My favorite food is pizza!');
+CommentModel.create('What is your favorite movie?');
+
+console.log('Database seeded!');
+
