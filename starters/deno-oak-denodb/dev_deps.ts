0145427fd1d24ed4a616bf555e5042fd166452ad
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-12-05 15:20:21 +0100

    [Deno] Generate Project (#592)
    * Generate Project
    
    * Add lock.json file to cache dependencies
    
    * removed test file

---
+++ starters/deno-oak-denodb/dev_deps.ts
@@ -0,0 +1,10 @@
+/**
+ * Exports all project devDepencies from the file
+ */
+
+export {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+export { testing } from "https://deno.land/x/oak@v11.1.0/mod.ts";
---
+++ starters/deno-oak-denodb/src/main.ts
@@ -0,0 +1,16 @@
+import { Application, Router, Context } from '../deps.ts';
+
+const app = new Application();
+const port = 3333;
+
+const router = new Router();
+router.get('/', ({ request, response }: Context) => {
+  response.body = `Hello world! from ${request.url}`;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`🚀 Application is running on: ${port}`);
+
+await app.listen({ port });
