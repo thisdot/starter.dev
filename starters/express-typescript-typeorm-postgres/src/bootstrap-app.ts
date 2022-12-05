785a2622bfc21c753ab8f147bde36e0dc2503399
Balázs Tápai <balazs.tapai1990@gmail.com>
2022-12-05 11:20:00 +0100

    fix: move health endpoint to match specification

--- starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
+++ starters/express-typescript-typeorm-postgres/src/bootstrap-app.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import expressOasGenerator, { SPEC_OUTPUT_FILE_BEHAVIOR } from 'express-oas-generator';
 import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import { HealthController } from './controllers/health/health.controller';
 import { apiRouter } from './controllers/router';
 import { corsMiddleware } from './middlewares/cors';
 
@@ -15,14 +16,15 @@ export function bootstrapApp(): Express {
   app.options('*', corsMiddleware);
   app.use(corsMiddleware);
 
+  app.use('/health', HealthController.router);
   app.use('/api', apiRouter);
   expressOasGenerator.handleRequests();
   app.use(genericErrorHandler);
   return app;
 }
 
-function genericErrorHandler(err, req: Request, res: Response) {
-  console.error('genericerrorhandler', err);
+function genericErrorHandler(err, req: Request, res: Response, next: NextFunction) {
+  console.error('An unexpected error occurred', err);
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send({ error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) });
--- starters/express-typescript-typeorm-postgres/src/controllers/router.ts
+++ starters/express-typescript-typeorm-postgres/src/controllers/router.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { HealthController } from './health/health.controller';
 import { TechnologyController } from './technology/technology.controller';
 
 const router = Router();
 
-router.use('/health', HealthController.router);
 router.use('/technology', TechnologyController.router);
 
 export const apiRouter = router;
