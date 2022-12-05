a76a986c0f3e068cf93620d46fa46b8e45910e90
Tyrel Chambers <tychambers3@gmail.com>
2022-12-05 13:11:06 -0500

    lock db version

--- starters/deno-oak-denodb/deps.ts
+++ starters/deno-oak-denodb/deps.ts
@@ -4,4 +4,4 @@
 
 export { Application, Router, Context } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
 
-export { DataTypes, Database, Model, PostgresConnector } from 'https://deno.land/x/denodb/mod.ts';
+export { DataTypes, Database, Model, PostgresConnector } from 'https://deno.land/x/denodb@v1.1.0/mod.ts';
