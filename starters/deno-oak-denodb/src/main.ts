531f23eb91bdead14b6162fd2294111a41d463ea
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-12-06 13:41:44 +0100

    [Deno] Configure API middleware (#603)
    * Added Cors and Dotenv
    
    * Fix: PR comments
    
    * Add comment

--- starters/deno-oak-denodb/src/main.ts
+++ starters/deno-oak-denodb/src/main.ts
@@ -1,16 +1,24 @@
-import { Application, Context, Router } from '../deps.ts';
+import { Application, config, Context, oakCors, Router } from '../deps.ts';
 
+const { API_HOST, CORS_ALLOWED_ORIGINS, PORT } = config({ safe: true });
 const app = new Application();
-const port = 3333;
+const port = +PORT || 3333;
 
 const router = new Router();
-router.get('/', async ({ request, response }: Context) => {
+router.get('/', ({ request, response }: Context) => {
   response.body = `Hello world! from ${request.url}`;
 });
 
+app.use(
+  oakCors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: CORS_ALLOWED_ORIGINS ? CORS_ALLOWED_ORIGINS.split(',') : '',
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  }),
+);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`🚀 Application is running on: ${port}`);
+console.log(`🚀 Application is running on: ${API_HOST}:${PORT}`);
 
 await app.listen({ port });
