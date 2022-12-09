2d959d13c3ba9b0219d1eee224d053137b3221d4
Dane Grant <danecando@gmail.com>
2022-12-09 10:39:01 -0500

    chore(cra-rxjs-styled-components): upgrade craco and use react 18

--- starters/cra-rxjs-styled-components/src/index.tsx
+++ starters/cra-rxjs-styled-components/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
