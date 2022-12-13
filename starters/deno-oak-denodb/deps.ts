0e614ab278cf4c8bb462cdafe0cd408ba8059fac
Tyrel Chambers <tychambers3@gmail.com>
2022-12-13 07:47:11 -0500

    add assert dep

--- starters/deno-oak-denodb/deps.ts
+++ starters/deno-oak-denodb/deps.ts
@@ -12,3 +12,4 @@ export { applyGraphQL, gql } from 'https://deno.land/x/oak_graphql@0.6.4/mod.ts'
 export { connect } from 'https://deno.land/x/redis@v0.27.4/mod.ts';
 export type { Redis, SetOpts } from 'https://deno.land/x/redis@v0.27.4/mod.ts';
 export type { GraphQLResolveInfo } from 'https://deno.land/x/graphql_deno@v15.0.0/mod.ts';
+export { assertEquals } from 'https://deno.land/std@0.167.0/testing/asserts.ts';
