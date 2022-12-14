5021c39c2c330faac699b09a54bfa2998cb14359
Dario Djuric <dario@thisdot.co>
2022-12-14 13:53:58 +0100

    Remove await

--- starters/deno-oak-denodb/src/graphql/resolvers/query_handler.ts
+++ starters/deno-oak-denodb/src/graphql/resolvers/query_handler.ts
@@ -4,24 +4,24 @@ import { Technologies } from '../../db/model/technology.ts';
 import { GraphqlContext, TechnologyArg } from '../interfaces/graphql_interfaces.ts';
 import { TechnologyRepository } from '../../db/repository/technology_repository.ts';
 
-export const getTechnologies = async (
+export const getTechnologies = (
 	_parent: unknown,
 	_args: TechnologyArg,
 	{ cache }: GraphqlContext,
 	info: GraphQLResolveInfo,
 ): Promise<Technologies[]> => {
-	return await useCache<Technologies[]>(info.fieldName, cache, async () => {
-		return await TechnologyRepository.getAll();
+	return useCache<Technologies[]>(info.fieldName, cache, () => {
+		return TechnologyRepository.getAll();
 	});
 };
 
-export const getTechnology = async (
+export const getTechnology = (
 	_parent: unknown,
 	{ id }: TechnologyArg,
 	{ cache }: GraphqlContext,
 	info: GraphQLResolveInfo,
 ): Promise<Technologies> => {
-	return await useCache<Technologies>(`${info.fieldName}:${id}`, cache, async () => {
-		return await TechnologyRepository.getById(id);
+	return useCache<Technologies>(`${info.fieldName}:${id}`, cache, () => {
+		return TechnologyRepository.getById(id);
 	});
 };
