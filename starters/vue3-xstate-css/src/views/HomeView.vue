8983f3753efaa18fc64f85235ba8183f572ceb15
LindaKat <lindakt16@gmail.com>
2022-12-15 17:56:01 -0600

    feat: implemented home page; removed vue boilerplate files no longer needed

--- starters/vue3-xstate-css/src/views/HomeView.vue
+++ starters/vue3-xstate-css/src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="title">
+      <h1>Vue 3 + XState + CSS</h1>
+    </div>
+    <div class="links">
+      <RouterLink to="/counter">See button example component</RouterLink>
+      <RouterLink to="/fetch">See data fetching example component</RouterLink>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.title {
+  width: 80%;
+  padding: 1% 2%;
+  margin: 0 auto 5%;
+  border-radius: 4px;
+  background: var(--lightBlue);
+}
+
+h1 {
+  color: var(--white);
+  text-align: center;
+}
+
+.links {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
