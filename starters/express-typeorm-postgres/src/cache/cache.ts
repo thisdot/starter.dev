a83858371a45a88cd04012eb4d1a736035e9c33c
Balázs Tápai <balazs.tapai1990@gmail.com>
2022-12-15 08:53:55 +0100

    fix: use built-in redis adapter from cachified v3.0.1

--- starters/express-typeorm-postgres/src/cache/cache.ts
+++ starters/express-typeorm-postgres/src/cache/cache.ts
@@ -1,7 +1,6 @@
-import { cachified } from 'cachified';
+import { cachified, redisCacheAdapter } from 'cachified';
 import { GetFreshValue } from 'cachified/dist/common';
 import * as process from 'process';
-import { redisCacheAdapterTtl } from './redis-cache-adapter-ttl';
 import { CACHE_HEALTH, cacheRedisClient } from './redis-cache-client';
 
 const FIVE_MINUTES_IN_MS = 300_000;
@@ -9,7 +8,7 @@ const REDIS_CACHE_TTL = process.env.REDIS_CACHE_TTL_IN_MS
 	? parseInt(process.env.REDIS_CACHE_TTL_IN_MS)
 	: FIVE_MINUTES_IN_MS;
 
-const cachifiedCache = redisCacheAdapterTtl(cacheRedisClient);
+const cachifiedCache = redisCacheAdapter(cacheRedisClient);
 
 export async function useCache<Value>(key: string, cb: GetFreshValue<Value>): Promise<Value> {
 	if (!CACHE_HEALTH.isConnected) {
