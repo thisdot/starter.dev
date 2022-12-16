044e365cf92fcdf97da6b76f5cfa6a56e47e7203
Linda Thompson <lindakt16@gmail.com>
2022-12-16 14:56:00 -0600

    [Vue+XState] Add home page (#661)
    * feat: implemented home page; removed vue boilerplate files no longer needed
    
    * fix: address pr feedback
    
    * Update starters/vue3-xstate-css/index.html
    
    Co-authored-by: Chris Trześniewski <k.trzesniewski@gmail.com>
    
    Co-authored-by: Chris Trześniewski <k.trzesniewski@gmail.com>

--- starters/vue3-xstate-css/src/router/index.ts
+++ starters/vue3-xstate-css/src/router/index.ts
@@ -9,14 +9,15 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    // NOTE: leaving for now so I have an example when I make the next component in ticket #512
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
   ],
 });
 
