52cc549381109b8b7623bfba0c9bc80ae78e9c33
Ian Mungai <iansam74@gmail.com>
2022-12-16 16:06:46 +0300

    fix: getGreeting at component layer

--- starters/svelte-kit-scss/src/routes/(examples)/api-example/+page.svelte
+++ starters/svelte-kit-scss/src/routes/(examples)/api-example/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import Greeting from '$lib/components/Greeting/Greeting.svelte';
+  import { getGreeting } from '$lib/helpers/greeting';
+  import { onMount } from 'svelte';
 
-  export let data: {
-    message: string;
-  };
+  let message: string = '';
+
+  onMount(async () => {
+		message = await getGreeting('This Dot Labs!');
+	});
 </script>
 
-<Greeting message={data.message} />
+<form method="POST" action="?/greeting">
+  <Greeting message={message} />
+</form>
+
