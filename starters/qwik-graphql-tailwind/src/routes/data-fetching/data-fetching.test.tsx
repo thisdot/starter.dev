9f60740e987f8e0fb84ef176030fb8915bdee8f9
Mattia Magi <mattia.magi91@gmail.com>
2022-12-16 18:03:49 +0100

    test: fix counter and data fetching tests

--- starters/qwik-graphql-tailwind/src/routes/data-fetching/data-fetching.test.tsx
+++ starters/qwik-graphql-tailwind/src/routes/data-fetching/data-fetching.test.tsx
@@ -1,5 +1,4 @@
-import { render } from '@builder.io/qwik';
-import { ElementFixture } from '@builder.io/qwik/testing';
+import { createDOM } from '@builder.io/qwik/testing';
 import { describe, expect, it, vi, beforeAll } from 'vitest';
 import { DataFetching, GET_GREETING } from './data-fetching';
 
@@ -13,23 +12,18 @@ beforeAll(() => {
 });
 
 describe('Fetching component', function () {
-  it('should assert true', async () => {
-    // just example test
-    expect(true).toBe(true);
-  });
-
   it('should render', async () => {
-    const fixture = new ElementFixture();
+    const { render, screen } = await createDOM();
     const spy = vi.spyOn(global, 'fetch').mockImplementationOnce(() => {
       return Promise.resolve({
         text: () => Promise.resolve('Hello World'),
       } as any);
     });
 
-    await render(fixture.host, <DataFetching />);
+    await render(<DataFetching />);
 
-    expect(fixture.host.querySelectorAll('input').length).toBe(1);
-    expect(fixture.host.querySelector('.text-5xl')?.textContent).toBe('Loading...');
+    expect(screen.querySelectorAll('input').length).toBe(1);
+    expect(screen.querySelector('.text-5xl')?.textContent).toBe('Loading...');
 
     // Wait for the fetch to be called.
     await new Promise((resolve) => setTimeout(resolve, 100));
@@ -42,7 +36,7 @@ describe('Fetching component', function () {
         query: GET_GREETING,
         variables: { greeting: 'there' },
       }),
-      signal: new AbortController().signal,
+      signal: expect.any(AbortSignal),
     });
   });
 });
