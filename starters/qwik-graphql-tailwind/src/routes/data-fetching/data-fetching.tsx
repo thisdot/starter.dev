f0991d38b366dd49947f3f55265f28a74106254e
Jan Kaiser <honza.kaiser@gmail.com>
2022-12-16 20:25:48 +0100

    fix: added styles to the data fetching example

--- starters/qwik-graphql-tailwind/src/routes/data-fetching/data-fetching.tsx
+++ starters/qwik-graphql-tailwind/src/routes/data-fetching/data-fetching.tsx
@@ -1,6 +1,5 @@
 import { component$, Resource, useResource$ } from '@builder.io/qwik';
 import { useQuery } from '../../utils/useQuery';
-import * as styles from './data-fetching.classNames';
 
 export const GET_GREETING = `
   query HelloQuery($greeting: String!) {
@@ -25,24 +24,32 @@ export const DataFetching = component$(() => {
   });
 
   return (
-    <div>
-      <div className={styles.textContainer}>
-        <Resource
-          value={greetingResource}
-          onPending={() => <>Loading...</>}
-          onRejected={(error) => <>Error: {error.message}</>}
-          onResolved={({ data }) => <div>Message: {data.hello}.</div>}
-        />
-      </div>
+    <div className="flex w-full text-xl justify-center">
+      <Resource
+        value={greetingResource}
+        onPending={() => <div className="text-left grow animate-pulse bg-gray-200 rounded-md">Loading...</div>}
+        onRejected={(error) => (
+          <div
+            className="grow border border-solid border-red-300 rounded bg-red-100 p-4 text-center text-red-500"
+            role="error-message"
+          >
+            Error: {error.message}
+          </div>
+        )}
+        onResolved={({ data }) => (
+          <>
+            <div className="mr-4">Message:</div>
+            <div className="text-left" role="display-message">
+              {data.hello}
+            </div>
+          </>
+        )}
+      />
     </div>
   );
 });
 
 export async function fetchGreeting(greeting: string, abortController?: AbortController): Promise<HelloResponse> {
-  if (!greeting) {
-    greeting = 'there';
-  }
-
   const { executeQuery$ } = useQuery(GET_GREETING);
 
   const resp = await executeQuery$({
