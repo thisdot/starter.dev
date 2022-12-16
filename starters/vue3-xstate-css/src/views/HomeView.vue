c124ebf551b5efec49e2a2b10afbd2ad988f9a46
Linda Thompson <lindakt16@gmail.com>
2022-12-16 14:56:00 -0600

    [Vue+XState] Add home page (#661)
    * feat: implemented home page; removed vue boilerplate files no longer needed
    
    * fix: address pr feedback
    
    * Update starters/vue3-xstate-css/index.html
    
    Co-authored-by: Chris Trześniewski <k.trzesniewski@gmail.com>
    
    Co-authored-by: Chris Trześniewski <k.trzesniewski@gmail.com>

--- starters/vue3-xstate-css/src/views/HomeView.vue
+++ starters/vue3-xstate-css/src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="home__title">
+      <h1>Vue 3 + XState + CSS</h1>
+    </div>
+    <div class="home__links">
+      <RouterLink to="/counter">See button example component</RouterLink>
+      <RouterLink to="/fetch">See data fetching example component</RouterLink>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.home__title {
+  width: 80%;
+  padding: 1% 2%;
+  margin: 0 auto 5%;
+  border-radius: 4px;
+  background: var(--lightBlue);
+}
+
+h1 {
+  color: var(--white);
+  text-align: center;
+}
+
+.home__links {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
