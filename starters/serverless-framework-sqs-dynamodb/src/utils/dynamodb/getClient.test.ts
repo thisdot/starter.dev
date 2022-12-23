53bfa4769668a712ea701ec4d75e0d163837249c
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-12-23 01:23:14 -0500

    add dynamodb client

---
+++ starters/serverless-framework-sqs-dynamodb/src/utils/dynamodb/getClient.test.ts
@@ -0,0 +1,93 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+
+describe('getClient', () => {
+	const OLD_ENV = process.env;
+	let subject;
+	let client: DynamoDB;
+
+	describe('when IS_OFFLINE is true', () => {
+		beforeAll(() => {
+			jest.resetModules();
+			client = require('@aws-sdk/client-dynamodb').DynamoDB;
+			const { getClient } = require('./getClient');
+			process.env = {
+				...OLD_ENV,
+				IS_OFFLINE: 'true',
+			};
+			subject = getClient();
+		});
+
+		afterAll(() => {
+			process.env = OLD_ENV;
+		});
+
+		it('returns an DynamoDB', () => {
+			expect(subject).toEqual(expect.any(client));
+		});
+
+		it('sets the endpoint to localhost', async () => {
+			expect(subject.config.isCustomEndpoint).toBe(true);
+			const { hostname } = await subject.config.endpoint();
+			expect(hostname).toMatch(/localhost/);
+		});
+	});
+
+	describe('when IS_OFFLINE is false', () => {
+		beforeAll(() => {
+			jest.resetModules();
+			client = require('@aws-sdk/client-dynamodb').DynamoDB;
+			const { getClient } = require('./getClient');
+			process.env = {
+				...OLD_ENV,
+				IS_OFFLINE: 'false',
+			};
+			subject = getClient();
+		});
+
+		afterAll(() => {
+			process.env = OLD_ENV;
+		});
+
+		it('returns an LambdaClient', () => {
+			expect(subject).toEqual(expect.any(client));
+		});
+
+		it('uses the default AWS Lambda endpoint', async () => {
+			expect(subject.config.isCustomEndpoint).toBe(false);
+		});
+	});
+
+	describe('when called twice', () => {
+		let dynamodb;
+
+		beforeAll(() => {
+			jest.resetModules();
+
+			dynamodb = require('@aws-sdk/client-dynamodb');
+			jest.doMock('@aws-sdk/client-dynamodb', () => ({
+				DynamoDB: jest.fn().mockImplementation(() => new dynamodb.DynamoDB({})),
+			}));
+			client = require('@aws-sdk/client-dynamodb').DynamoDB;
+
+			const { getClient } = require('./getClient');
+			process.env = {
+				...OLD_ENV,
+				IS_OFFLINE: 'true',
+			};
+			getClient();
+			subject = getClient();
+		});
+
+		afterAll(() => {
+			process.env = OLD_ENV;
+		});
+
+		it('runs the constructor once', () => {
+			expect(client).toHaveBeenCalledTimes(1);
+		});
+
+		it('returns a cached client', () => {
+			expect(subject).toEqual(expect.any(dynamodb.DynamoDB));
+		});
+	});
+});
---
+++ starters/serverless-framework-sqs-dynamodb/src/utils/dynamodb/getClient.ts
@@ -0,0 +1,28 @@
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
+import { isOffline } from '@utils/is-offline';
+
+let cachedClient: DynamoDB;
+
+export const getClient = () => {
+	if (cachedClient) {
+		return cachedClient;
+	}
+
+	const { REGION } = process.env;
+	const config: DynamoDBClientConfig = {
+		apiVersion: '2031',
+		region: REGION,
+	};
+
+	if (isOffline()) {
+		config.endpoint = 'http://localhost:8000';
+		// needs to be set if AWS credentials aren't configured
+		config.credentials = {
+			accessKeyId: 'DEFAULT_ACCESS_KEY',
+			secretAccessKey: 'DEFAULT_SECRET',
+		};
+	}
+
+	cachedClient = new DynamoDB(config);
+	return cachedClient;
+};
