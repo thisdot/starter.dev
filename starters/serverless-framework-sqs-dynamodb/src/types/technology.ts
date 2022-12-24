17423d119675bb55626401446a57fb0a141d57f3
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-12-24 16:55:28 -0500

    add create & update zod schemas + types

--- starters/serverless-framework-sqs-dynamodb/src/types/technology.ts
+++ starters/serverless-framework-sqs-dynamodb/src/types/technology.ts
@@ -1,6 +1,24 @@
+import { z } from 'zod';
+
 export type Technology = {
 	id: string;
 	displayName: string;
 	description: string;
-	url: string;
+	websiteUrl: string;
 };
+
+export const TechnologyCreateSchema = z.object({
+	displayName: z.string(),
+	description: z.string().min(10),
+	websiteUrl: z.string().url(),
+});
+
+export type TechnologyCreate = z.infer<typeof TechnologyCreateSchema>;
+
+export const TechnologyUpdateSchema = z.object({
+	displayName: z.string().optional(),
+	description: z.string().min(10).optional(),
+	websiteUrl: z.string().url().optional(),
+});
+
+export type TechnologyUpdate = z.infer<typeof TechnologyUpdateSchema>;
