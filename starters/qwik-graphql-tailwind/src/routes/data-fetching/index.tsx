09bb634c5623511387e1fc4fea9557193dc494dc
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2023-01-09 10:16:22 +0100

    remove white space (#737)

--- starters/qwik-graphql-tailwind/src/routes/data-fetching/index.tsx
+++ starters/qwik-graphql-tailwind/src/routes/data-fetching/index.tsx
@@ -6,7 +6,7 @@ import { DataFetching } from '~/components/data-fetching/data-fetching';
 
 export default component$(() => {
   return (
-    <div className="w-2/5 my-5 mx-auto text-center">
+    <div className="w-2/5 mx-auto text-center">
       <h1 className="text-[2rem] font-bold border-b-4 border-blue-600 py-4 my-5">
         Qwik + GraphQL Fetch Data using API route
       </h1>
