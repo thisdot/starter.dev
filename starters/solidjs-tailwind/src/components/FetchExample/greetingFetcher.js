28187e5f6b6a303a2acada00157bd365c8ddbb3f
Chinedu Ekene Okpala <allstackdev@gmail.com>
2023-01-12 08:26:45 +0100

    added input to received user customize name and refactor the component

--- starters/solidjs-tailwind/src/components/FetchExample/greetingFetcher.js
+++ starters/solidjs-tailwind/src/components/FetchExample/greetingFetcher.js
@@ -1,11 +1,8 @@
-const DEFAULT_MESSAGE = 'from This Dot Labs!';
-const greetingFetcher =
-  (message = DEFAULT_MESSAGE) =>
-  async () => {
-    const encodedMessage = encodeURIComponent(message);
-    const endpoint = `https://api.starter.dev/.netlify/functions/server/hello?greeting=${encodedMessage}`;
+const greetingFetcher = async (message) => {
+  const encodedMessage = encodeURIComponent(message);
+  const endpoint = `https://api.starter.dev/.netlify/functions/server/hello?greeting=${encodedMessage}`;
 
-    return fetch(endpoint).then((result) => result.text());
-  };
+  return await fetch(endpoint).then((result) => result.text());
+};
 
 export default greetingFetcher;
