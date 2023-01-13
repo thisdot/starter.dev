e6ee81775ee20e554f116d88524264512860b584
Chinedu Ekene  Okpala <36667083+AllStackDev1@users.noreply.github.com>
2023-01-13 11:23:37 +0100

    added input to received user customize name and refactor the component (#766)

--- starters/solidjs-tailwind/src/components/FetchExample/Greeting.jsx
+++ starters/solidjs-tailwind/src/components/FetchExample/Greeting.jsx
@@ -1,20 +1,59 @@
-import { createResource, Match, Switch } from 'solid-js';
+import { createEffect, createResource, createSignal } from 'solid-js';
 import greetingFetcher from './greetingFetcher';
 
 const Greeting = () => {
-  const [message] = createResource(greetingFetcher());
+  const [greeting, setGreeting] = createSignal('');
+  const [message, setMessage] = createSignal('');
+
+  const [resp] = createResource(
+    () => greeting(),
+    () => greetingFetcher(greeting())
+  );
+
+  createEffect(() => {
+    if (resp() && !resp.loading) {
+      setMessage(resp());
+    }
+  });
+
   return (
-    <Switch>
-      <Match when={message.error}>
-        <p class="text-red-500">There was an error loading your greeting :(</p>
-      </Match>
-      <Match when={!message.loading}>
-        <p>Message: {message()}</p>
-      </Match>
-      <Match when={message.loading}>
-        <div class="w-[80%] lg:w-[20%] mx-auto grow animate-pulse bg-gray-200 rounded-md p-3" />
-      </Match>
-    </Switch>
+    <div class="w-1/4 mx-auto">
+      <div class="pb-8 text-center">
+        <input
+          type="text"
+          value={greeting()}
+          placeholder="Who should the server greet?"
+          onChange={(ev) => setGreeting(ev.target.value)}
+          class="text-l w-96 max-w-full rounded border-0 bg-white px-4 py-4 shadow outline-none placeholder:italic focus:outline-none focus:ring"
+        />
+      </div>
+      {resp.error ? (
+        <div
+          class="grow rounded border border-solid border-red-300 bg-red-100 p-4 text-center text-red-500"
+          role="error-message"
+        >
+          Error:{' '}
+          <span class="text-red-500">
+            There was an error loading your greeting :(
+          </span>
+        </div>
+      ) : (
+        <div class="flex w-full justify-center text-xl">
+          {resp.loading ? (
+            <div class="grow animate-pulse rounded-md bg-gray-200 text-left">
+              Loading...
+            </div>
+          ) : (
+            <>
+              <div class="mr-4">Message:</div>
+              <div class="text-left" role="display-message">
+                {message()} from This Dot Labs!
+              </div>
+            </>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
