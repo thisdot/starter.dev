b33cd7bdf8d649c5eee368ddb0f70e629f6daab7
Dustin Goodman <dustin.s.goodman@gmail.com>
2023-01-15 13:51:22 -0600

    update processors to optimize record handler instantiation

--- starters/serverless-framework-sqs-dynamodb/src/handlers/example_job_processor.ts
+++ starters/serverless-framework-sqs-dynamodb/src/handlers/example_job_processor.ts
@@ -1,11 +1,11 @@
 import type { SQSHandler, SQSRecord } from 'aws-lambda';
 
+const recordHandler = async (record: SQSRecord) => {
+	console.log(JSON.stringify(record.body));
+};
+
 export const handler: SQSHandler = async (event) => {
 	console.log('Example Job Processor Handler initiated');
-
-	const recordHandler = async (record: SQSRecord) => {
-		console.log(JSON.stringify(record.body));
-	};
 
 	// Ensuring we await on all the promises is super important to avoid
 	// accidentally killing the lambda prior to processing being completed.
