e345ee67dfb73b1a314e8659ce82877780854a91
Mattia Magi <mattia.magi91@gmail.com>
2023-01-16 15:03:37 +0100

    fix: storybook

--- starters/qwik-graphql-tailwind/.storybook/main.ts
+++ starters/qwik-graphql-tailwind/.storybook/main.ts
@@ -1,5 +1,10 @@
 // Based on https://github.com/BuilderIO/qwik/discussions/787#discussioncomment-3715103
 
+import { mergeConfig } from 'vite';
+
+const viteTsconfig = require('vite-tsconfig-paths');
+const tsconfigPaths = viteTsconfig.default;
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -10,7 +15,9 @@ module.exports = {
   viteFinal: async (config, options) => {
     const { qwikVite: qwikVite } = await import('@builder.io/qwik/optimizer');
     config.plugins?.unshift(qwikVite());
-    return config;
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
   },
   features: {
     storyStoreV7: true,
