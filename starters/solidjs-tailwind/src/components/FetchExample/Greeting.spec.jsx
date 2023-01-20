42d7e1e80ad21fa0fdf07f81aa1764e0ea75d5ad
SDaian <scuarissid@gmail.com>
2023-01-20 18:09:08 +0100

    feat(solidjs-pod): Fix starter kit greeting component

--- starters/solidjs-tailwind/src/components/FetchExample/Greeting.spec.jsx
+++ starters/solidjs-tailwind/src/components/FetchExample/Greeting.spec.jsx
@@ -1,21 +1,52 @@
-import { render, screen } from 'solid-testing-library';
-import { describe, expect, it, beforeAll, afterAll, afterEach } from 'vitest';
+import { render } from 'solid-testing-library';
+import {
+  describe,
+  expect,
+  it,
+  beforeAll,
+  afterAll,
+  afterEach,
+  beforeEach,
+} from 'vitest';
 import 'whatwg-fetch';
 import { server } from '../../mock/serverSetup';
-import Greeting from './Greeting';
+import { Greeting } from '.';
+import { Router } from '@solidjs/router';
 
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 describe('Greeting', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = render(() => (
+      <Router>
+        <Greeting />
+      </Router>
+    ));
+
+    window.fetch = () => {
+      return new Promise((resolve) => {
+        resolve({
+          json: () => {
+            return new Promise((resolve) => {
+              resolve({
+                results: {
+                  text: () => 'daian',
+                },
+              });
+            });
+          },
+        });
+      });
+    };
+  });
   it('should mount', async () => {
-    const wrapper = await render(() => <Greeting />);
     expect(wrapper).toBeTruthy();
   });
 
   it('should show the mocked greeting', async () => {
-    await render(() => <Greeting />);
     const text = await screen.findByText('Hi Learner from This Dot Labs!');
     expect(text).toBeVisible();
   });
