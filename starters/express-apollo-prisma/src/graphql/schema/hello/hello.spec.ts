47b43559b74e34ad8a4d2c2f8f47cff3c3167ad9
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-01-23 16:21:55 +0300

    [express-apollo-prisma] 2. Add testing to the kit#775

---
+++ starters/express-apollo-prisma/src/graphql/schema/hello/hello.spec.ts
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+import { graphqlServer } from '../../graphql-server';
+import { Query } from '../generated/types';
+import assert from 'assert';
+
+type HelloQuery = Pick<Query, 'hello'>;
+
+describe('hello query', () => {
+	const MOCK_QUERY = gql`
+		query HelloWorldQuery($greeting: String!) {
+			hello(greeting: $greeting)
+		}
+	`;
+
+	it('returns expected result', async () => {
+		const MOCK_GREETING = 'MOCK_GREETING';
+
+		const subject = await graphqlServer.executeOperation<HelloQuery>({
+			query: MOCK_QUERY,
+			variables: {
+				greeting: MOCK_GREETING,
+			},
+		});
+		expect(subject.body.kind).toEqual('single');
+		assert(subject.body.kind === 'single');
+		expect(subject.body.singleResult.data).toEqual({
+			hello: `Hello, ${MOCK_GREETING}`,
+		});
+	});
+});
