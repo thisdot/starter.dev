1828ada7cfe1faac0e4580828ab6d29152a29d70
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-01-24 14:23:11 +0300

    [express-apollo-prisma] 1. Add store to the project (#843)
    * [express-apollo-prisma] 2. Add testing to the kit#775
    
    * pin deps
    
    * Excess script remove
    
    * [express-apollo-prisma] 1. Add store to the project#772
    
    * pin deps
    
    * format
    
    * add scripts for quick start

---
+++ starters/express-apollo-prisma/prisma/migrations/20230123160523_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(255) NOT NULL,
+    `postId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
