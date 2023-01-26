939052294a7fca4b2bb15af0680a722136c7cdf3
Mattia Magi <mattia.magi91@gmail.com>
2023-01-26 17:56:56 +0100

    fix name of the kit

--- starters/solidstart-tanstackquery-tailwind-modules/src/root.tsx
+++ starters/solidstart-tanstackquery-tailwind-modules/src/root.tsx
@@ -24,7 +24,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - TanStackRouter - TailwindCss - Css Modules - Kit</Title>
+        <Title>SolidStart - TanStackQuery - TailwindCss - Css Modules - Kit</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
