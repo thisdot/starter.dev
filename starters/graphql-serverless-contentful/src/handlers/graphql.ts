6983bee0b5bdab6f519bbd68f23f1aa11a4659b8
kodejuice <kodejuice@gmail.com>
2023-01-26 17:01:20 +0100

    add redis cache

--- starters/graphql-serverless-contentful/src/handlers/graphql.ts
+++ starters/graphql-serverless-contentful/src/handlers/graphql.ts
@@ -1,10 +1,23 @@
-import { ApolloServer, ApolloServerOptions, BaseContext } from '@apollo/server';
+import { ApolloServer, BaseContext } from '@apollo/server';
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
+import { KeyvAdapter } from '@apollo/utils.keyvadapter';
+import Keyv from 'keyv';
 import { typeDefs, resolvers } from '../schema';
+
+const {
+	REDIS_PORT = 6379,
+	REDIS_HOST = '127.0.0.1',
+	REDIS_USER = 'default',
+	REDIS_PASS = '',
+} = process.env;
+const client = new Keyv(
+	`redis://${REDIS_USER}:${REDIS_PASS}@${REDIS_HOST}:${REDIS_PORT}`
+);
 
 export const apolloServer = new ApolloServer({
 	typeDefs,
 	resolvers,
-} as ApolloServerOptions<BaseContext>);
+	cache: new KeyvAdapter(client),
+}) as unknown as ApolloServer<BaseContext>;
 
 export const server = startServerAndCreateLambdaHandler(apolloServer);
