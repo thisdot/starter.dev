75e7baf85a2a742052bcca836b5056bf6708e350
Mattia <32470461+Megio@users.noreply.github.com>
2023-01-26 12:55:53 +0100

    Solid fix: format code and add pre commit hook (#871)
    * added husky pre commit hook
    
    * fix pre commit
    
    * fix path in pre commit command
    
    * format files
    
    * added no console.log rule

--- starters/solidjs-tailwind/src/mock/serverSetup.js
+++ starters/solidjs-tailwind/src/mock/serverSetup.js
@@ -1,10 +1,6 @@
 import { setupServer } from 'msw/node';
 import greetingHandler from './greetingHandler';
 
-const server = setupServer(
-  greetingHandler
-)
+const server = setupServer(greetingHandler);
 
-export {
-  server,
-}
+export { server };
