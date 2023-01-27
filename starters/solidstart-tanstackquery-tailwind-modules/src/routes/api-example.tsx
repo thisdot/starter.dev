03635048ca74a10b13545a4afc03cbbc06f483c5
Jerry Hogan <jerry.hogan@thisdot.co>
2023-01-27 12:00:03 +0100

    routing, pages added

---
+++ starters/solidstart-tanstackquery-tailwind-modules/src/routes/api-example.tsx
@@ -0,0 +1,15 @@
+import { A } from "solid-start";
+
+export default function ApiExample () {
+  return (
+    <>
+      <h1>API Example Page</h1>
+        <A
+          href="/"
+          class="text-blue-800 hover:text-blue-500 transition-colors delay-100 underline"
+        >
+          Return Home
+        </A>
+    </>
+  )
+}
---
+++ starters/solidstart-tanstackquery-tailwind-modules/src/routes/counter.tsx
@@ -0,0 +1,15 @@
+import { A } from "solid-start";
+
+export default function Counter () {
+  return (
+    <>
+      <h1>Counter Page</h1>
+        <A
+          href="/"
+          class="text-blue-800 hover:text-blue-500 transition-colors delay-100 underline"
+        >
+          Return Home
+        </A>
+    </>
+  )
+}
