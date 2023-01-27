7403e2be4d3f5760195c724a3207158e4fea7013
kodejuice <kodejuice@gmail.com>
2023-01-27 17:07:24 +0100

    move redis client

--- starters/graphql-serverless-contentful/src/handlers/graphql.ts
+++ starters/graphql-serverless-contentful/src/handlers/graphql.ts
@@ -1,23 +1,13 @@
 import { ApolloServer, BaseContext } from '@apollo/server';
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 import { KeyvAdapter } from '@apollo/utils.keyvadapter';
-import Keyv from 'keyv';
 import { typeDefs, resolvers } from '../schema';
-
-const {
-	REDIS_PORT = 6379,
-	REDIS_HOST = '127.0.0.1',
-	REDIS_USER = 'default',
-	REDIS_PASS = '',
-} = process.env;
-const client = new Keyv(
-	`redis://${REDIS_USER}:${REDIS_PASS}@${REDIS_HOST}:${REDIS_PORT}`
-);
+import { redisClient } from '../utils/redis';
 
 export const apolloServer = new ApolloServer({
 	typeDefs,
 	resolvers,
-	cache: new KeyvAdapter(client),
+	cache: new KeyvAdapter(redisClient),
 }) as unknown as ApolloServer<BaseContext>;
 
 export const server = startServerAndCreateLambdaHandler(apolloServer);
--- starters/graphql-serverless-contentful/src/handlers/healthcheck.ts
+++ starters/graphql-serverless-contentful/src/handlers/healthcheck.ts
@@ -1,6 +1,9 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { redisClient } from '../utils/redis';
 
 export const handler: APIGatewayProxyHandler = async () => {
+	await redisClient.get(''); // should throw if not connected
+
 	return {
 		statusCode: 200,
 		body: 'Okay!',
