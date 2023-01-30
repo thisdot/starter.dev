9ef6617a69f76e0174de5afd5158be2bdb063a12
Mattia Magi <mattia.magi91@gmail.com>
2023-01-30 09:56:17 +0100

    wip

--- starters/solidstart-tanstackquery-tailwind-modules/src/components/Counter.tsx
+++ starters/solidstart-tanstackquery-tailwind-modules/src/components/Counter.tsx
@@ -3,12 +3,28 @@ import { createSignal } from "solid-js";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
+    <div class="flex flex-wrap lg:flex-nowrap justify-center gap-14 items-center">
+      <strong role="display-count" class="text-xl">
+        Count: {count()}
+      </strong>
     <button
-      data-testid="counter_btn"
-      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-      onClick={() => setCount(count() + 1)}
-    >
-      Clicks: {count()}
+    type="button"
+    class="bg-blue-500 text-white text-base font-medium px-4 py-2 rounded-md outline-none"
+    onClick={() => setCount(count() + 1)}>
+      Increment
     </button>
+    <button
+    type="button"
+    class="bg-blue-500 text-white text-base font-medium px-4 py-2 rounded-md outline-none"
+    onClick={() => setCount(count() - 1)}>
+      Decrement
+    </button>
+    <button
+    type="button"
+    class="bg-blue-500 text-white text-base font-medium px-4 py-2 rounded-md outline-none"
+    onClick={() => setCount(0)}>
+      Reset
+    </button>
+    </div>
   );
 }
