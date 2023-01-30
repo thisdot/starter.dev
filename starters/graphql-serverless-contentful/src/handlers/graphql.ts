eaf9c84645a28199768a51980e4fc0a895c20ec3
kodejuice <kodejuice@gmail.com>
2023-01-30 13:40:15 +0100

    cache ttl

--- starters/graphql-serverless-contentful/src/handlers/graphql.ts
+++ starters/graphql-serverless-contentful/src/handlers/graphql.ts
@@ -4,10 +4,15 @@ import { KeyvAdapter } from '@apollo/utils.keyvadapter';
 import { typeDefs, resolvers } from '../schema';
 import { redisClient } from '../utils/redis';
 
+const { REDIS_CACHE_TTL_SECONDS = 900 } = process.env;
+
 export const apolloServer = new ApolloServer({
 	typeDefs,
 	resolvers,
 	cache: new KeyvAdapter(redisClient),
+	persistedQueries: {
+		ttl: Number(REDIS_CACHE_TTL_SECONDS),
+	},
 }) as unknown as ApolloServer<BaseContext>;
 
 export const server = startServerAndCreateLambdaHandler(apolloServer);
