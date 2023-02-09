54ec74b46184c226880e48bf0f5ef46e015bf19e
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-02-09 20:43:47 +0300

    format

--- starters/serverless-apollo-contentful/src/utils/sqs/sendMessage.ts
+++ starters/serverless-apollo-contentful/src/utils/sqs/sendMessage.ts
@@ -1,4 +1,7 @@
-import { SendMessageCommand, SendMessageCommandOutput } from '@aws-sdk/client-sqs';
+import {
+	SendMessageCommand,
+	SendMessageCommandOutput,
+} from '@aws-sdk/client-sqs';
 import { getClient } from './client';
 import { getQueueUrl } from './getQueueUrl';
 
@@ -7,11 +10,13 @@ type Message = {
 };
 
 export type SendMessageResult = {
-	success: boolean,
-	data: SendMessageCommandOutput | string,
-}
+	success: boolean;
+	data: SendMessageCommandOutput | string;
+};
 
-export const sendMessage = async (message: Message): Promise<SendMessageResult> => {
+export const sendMessage = async (
+	message: Message
+): Promise<SendMessageResult> => {
 	const client = getClient();
 	const queueUrl = await getQueueUrl();
 	const command = new SendMessageCommand({
@@ -30,6 +35,6 @@ export const sendMessage = async (message: Message): Promise<SendMessageResult> 
 		return {
 			success: false,
 			data: error.message,
-		}
+		};
 	}
 };
