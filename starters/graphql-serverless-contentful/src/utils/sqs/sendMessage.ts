d1281d027ca0f84b96f7babf126149e1cf57d8d9
Ian Mungai <iansam74@gmail.com>
2023-02-09 14:37:42 +0300

    chore: sendMessage test

--- starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
+++ starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
@@ -1,4 +1,7 @@
-import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import {
+	SendMessageCommand,
+	SendMessageCommandOutput,
+} from '@aws-sdk/client-sqs';
 import { getClient } from './client';
 import { getQueueUrl } from './getQueueUrl';
 
@@ -6,7 +9,14 @@ type Message = {
 	[key: string]: string | number | boolean | null;
 };
 
-export const sendMessage = async (message: Message) => {
+export type SendMessageResult = {
+	success: boolean;
+	data: SendMessageCommandOutput | string;
+};
+
+export const sendMessage = async (
+	message: Message
+): Promise<SendMessageResult> => {
 	const client = getClient();
 	const queueUrl = await getQueueUrl();
 	const command = new SendMessageCommand({
@@ -20,10 +30,11 @@ export const sendMessage = async (message: Message) => {
 			success: true,
 			data,
 		};
-	} catch (error: any) {
+	} catch (err) {
+		const error = err as Error;
 		return {
 			success: false,
-			data: error?.message,
+			data: error.message,
 		};
 	}
 };
