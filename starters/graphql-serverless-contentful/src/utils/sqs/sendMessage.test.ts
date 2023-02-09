a20fede485318e2b4a8217119c803b3062808e65
Ian Mungai <iansam74@gmail.com>
2023-02-09 21:16:43 +0300

    chore: update test sendMessage

--- starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.test.ts
+++ starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.test.ts
@@ -1,73 +1,108 @@
-import { sendMessage } from './sendMessage';
+import { SendMessageCommandOutput } from '@aws-sdk/client-sqs';
+import { ResponseMetadata } from '@aws-sdk/types/dist-types';
+import { sendMessage, SendMessageResult } from './sendMessage';
 import { getClient } from './client';
+import { getQueueUrl } from './getQueueUrl';
+
+const MOCK_GET_CLIENT = getClient as jest.Mock;
+const MOCK_GET_QUEUE_URL = getQueueUrl as jest.Mock;
+const MOCK_QUEUE_URL = 'MOCK_QUEUE_URL';
+const MOCK_METADATA: ResponseMetadata = {
+	httpStatusCode: 200,
+};
+const MOCK_DATA: SendMessageCommandOutput = {
+	$metadata: MOCK_METADATA,
+};
+const MOCK_ERROR_MESSAGE = 'MOCK_ERROR_MESSAGE';
 
 jest.mock('./client', () => ({
-	getClient: jest.fn().mockReturnValue({
-		send: jest.fn().mockResolvedValue({}),
-	}),
+	getClient: jest.fn(),
 }));
 
 jest.mock('./getQueueUrl', () => ({
-	getQueueUrl: jest.fn().mockResolvedValue('queue-url'),
+	getQueueUrl: jest.fn(),
 }));
 
-describe('sendMessage', () => {
-	it('should return an object with success true and the data returned by the send method', async () => {
-		const message = { key: 'value' };
-		const result = await sendMessage(message);
-		expect(result).toEqual({
-			success: true,
-			data: {},
-		});
-		expect(getClient).toHaveBeenCalled();
-		expect(getClient().send).toHaveBeenCalledWith(
-			expect.objectContaining({
-				input: {
-					QueueUrl: 'queue-url',
-					MessageBody: JSON.stringify(message),
-				},
-			})
-		);
-	});
+describe('.sendMessage', () => {
+	const message = { key: 'value' };
+	let result: SendMessageResult;
 
-	it('should return an object with success false and the error message if the send method throws an error', async () => {
-		const errorMessage = 'error message';
-		jest
-			.spyOn(getClient(), 'send')
-			.mockRejectedValue(new Error(errorMessage) as never);
-		const message = { key: 'value' };
-		const result = await sendMessage(message);
-		expect(result).toEqual({
-			success: false,
-			data: errorMessage,
-		});
-		expect(getClient).toHaveBeenCalled();
-		expect(getClient().send).toHaveBeenCalledWith(
-			expect.objectContaining({
-				input: {
-					QueueUrl: 'queue-url',
-					MessageBody: JSON.stringify(message),
-				},
-			})
-		);
-	});
+	describe('when called with correct argument', () => {
+		describe('and SQSClient.send returns result', () => {
+			beforeAll(async () => {
+				MOCK_GET_CLIENT.mockReturnValue({
+					send: jest.fn().mockResolvedValue(MOCK_DATA),
+				});
+				MOCK_GET_QUEUE_URL.mockResolvedValue(MOCK_QUEUE_URL);
+				result = await sendMessage(message);
+			});
 
-	it('should return a string representation of the error if it is not an instance of Error', async () => {
-		const error = { message: 'error message' };
-		jest
-			.spyOn(getClient(), 'send')
-			.mockRejectedValue(new Error(error.message) as never);
-		const message = { key: 'value' };
-		const result = await sendMessage(message);
-		expect(result).toEqual({ data: 'error message', success: false });
-		expect(getClient).toHaveBeenCalled();
-		expect(getClient().send).toHaveBeenCalledWith(
-			expect.objectContaining({
-				input: {
-					QueueUrl: 'queue-url',
-					MessageBody: JSON.stringify(message),
-				},
-			})
-		);
+			afterAll(() => {
+				MOCK_GET_CLIENT.mockReset();
+				MOCK_GET_QUEUE_URL.mockReset();
+			});
+
+			it('calls getClient', () => {
+				expect(MOCK_GET_CLIENT).toHaveBeenCalledTimes(1);
+			});
+			it('calls getQueueUrl', () => {
+				expect(MOCK_GET_QUEUE_URL).toHaveBeenCalledTimes(1);
+			});
+			it('calls SQSClient.send with expected command', () => {
+				expect(getClient().send).toHaveBeenCalledTimes(1);
+				expect(getClient().send).toHaveBeenCalledWith(
+					expect.objectContaining({
+						input: {
+							QueueUrl: MOCK_QUEUE_URL,
+							MessageBody: JSON.stringify(message),
+						},
+					})
+				);
+			});
+			it('returns expected result', () => {
+				expect(result).toEqual({
+					success: true,
+					data: MOCK_DATA,
+				});
+			});
+		});
+
+		describe('and SQSClient.send throws error', () => {
+			beforeAll(async () => {
+				MOCK_GET_CLIENT.mockReturnValue({
+					send: jest.fn().mockRejectedValue(new Error(MOCK_ERROR_MESSAGE)),
+				});
+				MOCK_GET_QUEUE_URL.mockResolvedValue(MOCK_QUEUE_URL);
+				result = await sendMessage(message);
+			});
+
+			afterAll(() => {
+				MOCK_GET_CLIENT.mockReset();
+				MOCK_GET_QUEUE_URL.mockReset();
+			});
+			it('calls getClient', () => {
+				expect(MOCK_GET_CLIENT).toHaveBeenCalledTimes(1);
+			});
+			it('calls getQueueUrl', () => {
+				expect(MOCK_GET_QUEUE_URL).toHaveBeenCalledTimes(1);
+			});
+			it('calls SQSClient.send with expected command', () => {
+				expect(getClient().send).toHaveBeenCalledTimes(1);
+				expect(getClient().send).toHaveBeenCalledWith(
+					expect.objectContaining({
+						input: {
+							QueueUrl: MOCK_QUEUE_URL,
+							MessageBody: JSON.stringify(message),
+						},
+					})
+				);
+			});
+			it('returns expected result', () => {
+				expect(result).toEqual({
+					success: false,
+					data: MOCK_ERROR_MESSAGE,
+				});
+			});
+		});
 	});
 });
