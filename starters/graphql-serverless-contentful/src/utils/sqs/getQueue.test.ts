f919e0737cbe85cf40f42785e2ad456a0043aca1
Ian Mungai <iansam74@gmail.com>
2023-02-09 13:32:33 +0300

    chore: getQueue test

---
+++ starters/graphql-serverless-contentful/src/utils/sqs/getQueue.test.ts
@@ -0,0 +1,41 @@
+import { getQueueUrl } from './getQueueUrl';
+import { GetQueueUrlCommand } from '@aws-sdk/client-sqs';
+
+describe('getQueueUrl', () => {
+	beforeEach(() => {
+		process.env.JOB_QUEUE = 'test-queue';
+	});
+
+	afterEach(() => {
+		delete process.env.JOB_QUEUE;
+	});
+
+	it('should return the URL of the queue when the queue exists', async () => {
+		const mockSend = jest
+			.fn()
+			.mockResolvedValue({ QueueUrl: 'https://test-queue-url' });
+		jest.mock('./client', () => ({
+			getClient: jest.fn().mockReturnValue({ send: mockSend }),
+		}));
+
+		const result = await getQueueUrl();
+		expect(result).toEqual('https://test-queue-url');
+		expect(mockSend).toHaveBeenCalledWith(expect.any(GetQueueUrlCommand));
+	});
+
+	it('should throw an error when the queue does not exist', async () => {
+		const mockSend = jest.fn().mockRejectedValue(new Error('Queue not found'));
+		jest.mock('./client', () => ({
+			getClient: jest.fn().mockReturnValue({ send: mockSend }),
+		}));
+
+		await expect(getQueueUrl()).rejects.toThrowError('Queue not found');
+		expect(mockSend).toHaveBeenCalledWith(expect.any(GetQueueUrlCommand));
+	});
+
+	it('should throw an error when the JOB_QUEUE environment variable is not set', async () => {
+		delete process.env.JOB_QUEUE;
+
+		await expect(getQueueUrl()).rejects.toThrowError('Unable to find queue.');
+	});
+});
