09cf70c7ebb08d640aeaa2f6c2cc4b555c6715f5
Ian Sam Mungai <ian.mungai@thisdot.co>
2023-02-10 12:32:38 +0300

    [serverless-apollo-contentful] Unit test for utils/sqs/sendMessage.ts (#982)
    * chore: sendMessage test
    
    * chore: sendMessage test
    
    * chore: update test sendMessage
    
    * chore: lock deps
    
    * chore: change expexted argument
    
    * fix: calls SQSClient.send with expected command

--- starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
+++ starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
@@ -1,12 +1,22 @@
-import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import {
+	SendMessageCommand,
+	SendMessageCommandOutput,
+} from '@aws-sdk/client-sqs';
 import { getClient } from './client';
 import { getQueueUrl } from './getQueueUrl';
 
-type Message = {
+export type Message = {
 	[key: string]: string | number | boolean | null;
 };
 
-export const sendMessage = async (message: Message) => {
+export type SendMessageResult = {
+	success: boolean;
+	data: SendMessageCommandOutput | string;
+};
+
+export const sendMessage = async (
+	message: Message
+): Promise<SendMessageResult> => {
 	const client = getClient();
 	const queueUrl = await getQueueUrl();
 	const command = new SendMessageCommand({
@@ -20,10 +30,11 @@ export const sendMessage = async (message: Message) => {
 			success: true,
 			data,
 		};
-	} catch (error: any) {
+	} catch (err) {
+		const error = err as Error;
 		return {
 			success: false,
-			data: error?.message,
+			data: error.message,
 		};
 	}
 };
