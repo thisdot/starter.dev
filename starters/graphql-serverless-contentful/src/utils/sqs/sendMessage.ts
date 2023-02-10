de906408257a903979ae47b6cacb0734e0e0b94f
Ian Sam Mungai <iansam74@gmail.com>
2023-02-10 13:02:23 +0300

    Revert "[serverless-apollo-contentful] Unit test for utils/sqs/sendMessage.ts (#982)"
    This reverts commit 09cf70c7ebb08d640aeaa2f6c2cc4b555c6715f5.

--- starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
+++ starters/graphql-serverless-contentful/src/utils/sqs/sendMessage.ts
@@ -1,22 +1,12 @@
-import {
-	SendMessageCommand,
-	SendMessageCommandOutput,
-} from '@aws-sdk/client-sqs';
+import { SendMessageCommand } from '@aws-sdk/client-sqs';
 import { getClient } from './client';
 import { getQueueUrl } from './getQueueUrl';
 
-export type Message = {
+type Message = {
 	[key: string]: string | number | boolean | null;
 };
 
-export type SendMessageResult = {
-	success: boolean;
-	data: SendMessageCommandOutput | string;
-};
-
-export const sendMessage = async (
-	message: Message
-): Promise<SendMessageResult> => {
+export const sendMessage = async (message: Message) => {
 	const client = getClient();
 	const queueUrl = await getQueueUrl();
 	const command = new SendMessageCommand({
@@ -30,11 +20,10 @@ export const sendMessage = async (
 			success: true,
 			data,
 		};
-	} catch (err) {
-		const error = err as Error;
+	} catch (error: any) {
 		return {
 			success: false,
-			data: error.message,
+			data: error?.message,
 		};
 	}
 };
