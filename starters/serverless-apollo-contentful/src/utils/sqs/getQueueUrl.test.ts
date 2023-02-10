28b0d95d08fa239322fa4439d3d9e6e0bf70f0ea
Ian Mungai <iansam74@gmail.com>
2023-02-10 16:29:34 +0300

    chore: getQueueUrl test

--- starters/serverless-apollo-contentful/src/utils/sqs/getQueueUrl.test.ts
+++ starters/serverless-apollo-contentful/src/utils/sqs/getQueueUrl.test.ts
@@ -55,38 +55,18 @@ describe('.getQueueUrl', () => {
 		});
 
 		describe('and client.send throws error', () => {
-			beforeAll(async () => {
+			beforeAll(() => {
 				MOCK_GET_CLIENT.mockReturnValue({
 					send: jest.fn().mockRejectedValue(new Error(MOCK_ERROR_MESSAGE)),
 				});
-				result = await getQueueUrl();
 			});
 
 			afterAll(() => {
 				MOCK_GET_CLIENT.mockReset();
 			});
 
-			it('calls getClient', () => {
-				expect(getClient).toHaveBeenCalledTimes(1);
-			});
-
-			it('calls client.send with expected command', () => {
-				const EXPECTED_INPUT = {
-					QueueName: process.env.JOB_QUEUE,
-				};
-				const mockSend = MOCK_GET_CLIENT.mock.results[0].value.send;
-				const expectedArgument = mockSend.mock.calls[0][0];
-
-				expect(mockSend).toHaveBeenCalledTimes(1);
-				expect(expectedArgument).toBeInstanceOf(GetQueueUrlCommand);
-				expect(expectedArgument.input).toEqual(EXPECTED_INPUT);
-			});
-
 			it('throws expected error', async () => {
-				expect(result).toBe(undefined);
-				await expect(async () => await getQueueUrl()).rejects.toThrow(
-					MOCK_ERROR_MESSAGE
-				);
+				await expect(getQueueUrl()).rejects.toThrow(MOCK_ERROR_MESSAGE);
 			});
 		});
 	});
