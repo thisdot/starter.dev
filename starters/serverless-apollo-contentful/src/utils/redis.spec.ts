85f6e3ddff55aa3cd435597ea0af9146bdf25eeb
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-02-10 12:55:12 +0300

    [serverless-apollo-contentful] Unit test for utils/redis.ts (#1014)
    * [serverless-apollo-contentful] Unit test for utils/redis.ts#970
    
    * Update redis.spec.ts

---
+++ starters/serverless-apollo-contentful/src/utils/redis.spec.ts
@@ -0,0 +1,38 @@
+import assert from 'assert';
+import Keyv from 'keyv';
+import { redisClient } from './redis';
+
+jest.mock('./redis', () => {
+	// Original env snapshot 
+	const _originalEnv = process.env;
+	process.env = {
+		REDIS_PORT: '1234',
+		REDIS_HOST: 'MOCK_HOST',
+		REDIS_USER: 'MOCK_USER',
+		REDIS_PASS: 'MOCK_PASS',
+	};
+		// Require the original module to not be mocked...
+	const originalModule =
+		jest.requireActual<typeof import('./redis')>('./redis');
+
+	return {
+		__esModule: true, // Use it when dealing with esModules
+		...originalModule,
+		_originalEnv, // Store original env to reset changes after tests
+	};
+});
+
+describe('./redis', () => {
+	afterAll(async () => {
+		const mockedModule = await import('./redis');
+		assert('_originalEnv' in mockedModule);
+		process.env = <NodeJS.ProcessEnv>mockedModule._originalEnv;
+	});
+
+	it('exports valid redis client', () => {
+		expect(redisClient).toBeInstanceOf(Keyv);
+		expect(redisClient.opts.uri).toStrictEqual(
+			'redis://MOCK_USER:MOCK_PASS@MOCK_HOST:1234'
+		);
+	});
+});
