86f764e361f6582a711577a8ccdcae3f37786f06
Ian Mungai <iansam74@gmail.com>
2023-02-13 23:22:42 +0300

    chore: fix redis-healthcheck.spec

--- starters/serverless-apollo-contentful/src/utils/redis/redis-healthcheck.spec.ts
+++ starters/serverless-apollo-contentful/src/utils/redis/redis-healthcheck.spec.ts
@@ -1,30 +1,36 @@
 import { getRedisHealth } from './redis-healthcheck';
 import { redisClient } from './redis';
 
-const MOCK_REDIS_CLIENT = redisClient as unknown as jest.Mock;
-
-jest.mock('./contentful', () => ({
-	redisClient: jest.fn(),
+const MOCK_REDIS_CLIENT = redisClient.get as jest.Mock;
+jest.mock('./redis', () => ({
+	redisClient: {
+		get: jest.fn(),
+	},
 }));
 
 describe('.healthcheck', () => {
 	describe('when connection is successful', () => {
 		let result: boolean;
+
 		beforeAll(async () => {
 			MOCK_REDIS_CLIENT.mockResolvedValue('');
 			result = await getRedisHealth();
 		});
-		it('should return 200', () => {
+
+		it('should return true', () => {
 			expect(result).toEqual(true);
 		});
 	});
+
 	describe('when connection is unsuccessful', () => {
 		let result: boolean;
+
 		beforeAll(async () => {
 			MOCK_REDIS_CLIENT.mockRejectedValue(undefined);
 			result = await getRedisHealth();
 		});
-		it('should return 500', () => {
+
+		it('should return false', () => {
 			expect(result).toEqual(false);
 		});
 	});
