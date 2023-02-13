c51b194e79280d7a44cce0018abb3b1a31b9ba52
Ian Mungai <iansam74@gmail.com>
2023-02-13 11:58:51 +0300

    chore: healthcheck contentful

--- starters/serverless-apollo-contentful/src/handlers/healthcheck.ts
+++ starters/serverless-apollo-contentful/src/handlers/healthcheck.ts
@@ -1,8 +1,15 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { getEnvironment } from '../utils/contentful';
 import { redisClient } from '../utils/redis';
+
+type HealthCheckResult = {
+	cacheDatabase: boolean;
+	contentful: boolean;
+};
 
 export const handler: APIGatewayProxyHandler = async () => {
 	let cacheDatabase: boolean;
+	let contentful: boolean;
 	try {
 		await redisClient.get('');
 		cacheDatabase = true;
@@ -10,8 +17,23 @@ export const handler: APIGatewayProxyHandler = async () => {
 		cacheDatabase = false;
 	}
 
+	try {
+		await getEnvironment();
+		contentful = true;
+	} catch {
+		contentful = false;
+	}
+
+	const result: HealthCheckResult = {
+		cacheDatabase,
+		contentful,
+	};
+
+	const hasFailedCheck = Object.values(result).includes(false);
+	const statusCode = hasFailedCheck ? 503 : 200;
+
 	return {
-		statusCode: cacheDatabase ? 200 : 503,
+		statusCode: statusCode,
 		body: JSON.stringify({ cacheDatabase }),
 		headers: {
 			'Content-Type': 'application/json',
