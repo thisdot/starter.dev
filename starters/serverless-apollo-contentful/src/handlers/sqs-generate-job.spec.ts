ff147ef0a35f89e01a0642060d8a305a46d420d4
kodejuice <kodejuice@gmail.com>
2023-02-14 16:14:20 +0100

    redundant

--- starters/serverless-apollo-contentful/src/handlers/sqs-generate-job.spec.ts
+++ starters/serverless-apollo-contentful/src/handlers/sqs-generate-job.spec.ts
@@ -1,14 +1,8 @@
 import { handler } from './sqs-generate-job';
 import { sendMessage } from '../utils/sqs';
 
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { mockAWSLambdaHandlerContext } from '../utils/test/mocks';
-
-const MOCK_RECORDS_COUNT = 1;
-const MOCK_CONTEXT = mockAWSLambdaHandlerContext();
-const MOCK_CALLBACK = jest.fn();
+import { APIGatewayProxyEvent, Callback, Context } from 'aws-lambda';
 const MOCK_SEND_MESSAGE = sendMessage as jest.Mock;
-let MOCK_API_GATEWAY_PROXY_EVENT: APIGatewayProxyEvent;
 
 jest.mock('../utils/sqs', () => ({
 	sendMessage: jest.fn().mockReturnValue({
@@ -23,14 +17,14 @@ describe('.handler', () => {
 	describe('when called with event', () => {
 		beforeAll(async () => {
 			subject = await handler(
-				MOCK_API_GATEWAY_PROXY_EVENT,
-				MOCK_CONTEXT,
-				MOCK_CALLBACK
+				{} as APIGatewayProxyEvent,
+				{} as Context,
+				{} as Callback
 			);
 		});
 
 		it('calls sendMessage with expected arguments', () => {
-			expect(MOCK_SEND_MESSAGE).toHaveBeenCalledTimes(MOCK_RECORDS_COUNT);
+			expect(MOCK_SEND_MESSAGE).toHaveBeenCalledTimes(1);
 			const actualCallsArgs = MOCK_SEND_MESSAGE.mock.calls[0][0];
 			expect(actualCallsArgs).toHaveProperty('message');
 			expect(actualCallsArgs.message).toEqual('Hello World!');
