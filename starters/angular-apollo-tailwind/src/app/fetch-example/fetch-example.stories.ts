d376243fac9273d0d2131b176d67c4d9a08a58aa
William Hutt <whutt1994@gmail.com>
2023-02-15 07:07:14 -0800

    chore: updates Angular-apollo-tailwind for minor updates/patches (#1013)

--- starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.stories.ts
+++ starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.stories.ts
@@ -4,20 +4,20 @@ import { ApolloModule } from 'apollo-angular';
 import { FetchExampleComponent } from './fetch-example.component';
 
 export default {
-    title: 'Example/Fetch Example',
-    component: FetchExampleComponent,
-    decorators: [
-        moduleMetadata({
-            declarations: [FetchExampleComponent],
-            imports: [ApolloModule],
-        }),
-    ],
+  title: 'Example/Fetch Example',
+  component: FetchExampleComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [FetchExampleComponent],
+      imports: [ApolloModule],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<FetchExampleComponent> = (
-    args: FetchExampleComponent
+  args: FetchExampleComponent
 ) => ({
-    props: args,
+  props: args,
 });
 
 export const FetchExample = Template.bind({});
