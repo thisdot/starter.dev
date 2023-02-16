3e3faf9b85f41cd371995cb99d0c7cd022d5cd30
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-02-17 00:40:22 +0300

    [serverless-apollo-contentful] Clean up src/schema/technology/technology.resolvers.test.ts (#1063)
    * [serverless-apollo-contentful] Clean up src/schema/technology/technology.resolvers.test.ts#1045
    
    * Update technology.resolver.test.ts

--- starters/serverless-apollo-contentful/src/schema/technology/technology.resolver.spec.ts
+++ starters/serverless-apollo-contentful/src/schema/technology/technology.resolver.spec.ts
@@ -1,193 +1,280 @@
-import gql from 'graphql-tag';
-import { apolloServer } from '../../handlers/graphql';
-import { GraphQLResponse } from '@apollo/server/src/externalTypes';
+import { technologyResolvers } from './technology.resolvers';
+import { TechnologyModel } from '../../models/TechnologyModel';
 import assert from 'assert';
-import { Query } from '../../generated/graphql';
-import { getEnvironment } from '../../utils/contentful';
-import { Environment } from 'contentful-management';
+import {
+	Maybe,
+	MutationCreateTechnologyArgs,
+	MutationUpdateTechnologyArgs,
+	RequireFields,
+	ResolversParentTypes,
+	ResolverTypeWrapper,
+	Technology,
+} from '../../generated/graphql';
+import { BaseContext } from '@apollo/server';
+import { GraphQLResolveInfo } from 'graphql';
+import { mockEntry, mockGraphQLResolveInfo } from '../../utils/test/mocks';
 
-type TechnologyQuery = Pick<Query, 'technology'>;
 
-jest.mock('../../utils/contentful', () => ({
-	getEnvironment: jest.fn(),
+jest.mock('../../models/TechnologyModel', () => ({
+	TechnologyModel: {
+		getAll: jest.fn(),
+		get: jest.fn(),
+		create: jest.fn(),
+		update: jest.fn(),
+		test: 'YE'
+	},
 }));
 
-const MOCK_TECHNOLOGIES = [
-	{
-		id: '4f997875-a96d-4c84-ba43-39afd764168d',
-		displayName: 'GraphQL',
-		description:
-			'GraphQL provides a strong-typing system to better understand and utilize our API to retrieve and interact with our data.',
-		url: 'https://graphql.framework.dev/',
-	},
-	{
-		id: '2e6c7d25-8b03-4348-8329-cc4185225c71',
-		displayName: 'Node.js',
-		description:
-			'Node.js® is an open-source, cross-platform JavaScript runtime environment.',
-		url: 'https://nodejs.framework.dev/',
-	},
-];
-const MOCK_CONTENTFUL_ENTRIES = {
-	items: MOCK_TECHNOLOGIES.map(({ id, displayName, description, url }) => ({
-		sys: {
-			id,
-		},
-		fields: {
-			displayName: {
-				'en-US': displayName,
-			},
-			description: {
-				'en-US': description,
-			},
-			url: {
-				'en-US': url,
-			},
-		},
-		publish: () => undefined,
-		update: () => undefined,
-	})),
+type TechnologyModelStaticPartial = Pick<
+	typeof TechnologyModel,
+	Exclude<keyof typeof TechnologyModel, 'prototype'>
+>;
+type TechnologyModelStaticPartialMock = {
+	[K in keyof TechnologyModelStaticPartial]: jest.MockedFn<
+		TechnologyModelStaticPartial[K]
+	>;
 };
 
-const getEntries = jest.fn(() => MOCK_CONTENTFUL_ENTRIES);
-const getEntry = jest.fn(() => MOCK_CONTENTFUL_ENTRIES.items[1]);
-const createEntry = jest.fn(() => MOCK_CONTENTFUL_ENTRIES.items[0]);
+const MOCK_TECHNOLOGY_MODEL_STATIC =
+	TechnologyModel as unknown as TechnologyModelStaticPartialMock;
 
-const mockGetEnvironment = getEnvironment as jest.MockedFunction<
-	typeof getEnvironment
->;
-mockGetEnvironment.mockResolvedValue({
-	getEntry,
-	getEntries,
-	createEntry,
-} as unknown as Environment);
+const MOCK_PARENT_QUERY: ResolversParentTypes['Query'] = {};
+const MOCK_PARENT_MUTATION: ResolversParentTypes['Mutation'] = {};
 
-describe('technology queries and mutations', () => {
-	describe('query technologies', () => {
-		it('returns all technologies', async () => {
-			const query = gql`
-				query {
-					technology {
-						id
-						displayName
-						description
-						url
-					}
-				}
-			`;
+const MOCK_CONTEXT: BaseContext = {};
+const MOCK_RESOLVE_INFO: GraphQLResolveInfo = mockGraphQLResolveInfo();
 
-			const subject: GraphQLResponse =
-				await apolloServer.executeOperation<TechnologyQuery>({ query });
+const MOCK_ENTRY_1 = mockEntry('MOCK_ID_1', {
+	description: {
+		'en-US': 'MOCK_DESCRIPTION_1',
+	},
+	displayName: {
+		'en-US': 'MOCK_DISPLAY_NAME_1',
+	},
+	url: {
+		'en-US': 'MOCK_URL_1',
+	},
+});
 
-			assert(subject.body.kind === 'single');
-			expect(subject.body.singleResult.errors).toBeUndefined();
-			expect(mockGetEnvironment).toHaveBeenCalledTimes(1);
-			expect(getEntries).toHaveBeenCalledWith({ content_type: 'technology' });
-			expect(subject.body.singleResult.data?.technology).toEqual(
-				MOCK_TECHNOLOGIES
-			);
-		});
-	});
+const MOCK_ENTRY_2 = mockEntry('MOCK_ID_2', {
+	description: {
+		'en-US': 'MOCK_DESCRIPTION_2',
+	},
+	displayName: {
+		'en-US': 'MOCK_DISPLAY_NAME_2',
+	},
+	url: {
+		'en-US': 'MOCK_URL_2',
+	},
+});
 
-	describe('query technology by id', () => {
-		it('returns the technology with the given id', async () => {
-			const query = gql`
-				query TechnologyQuery($id: ID!) {
-					technology(id: $id) {
-						id
-						displayName
-						description
-						url
-					}
-				}
-			`;
+describe('technologyResolvers', () => {
+	describe('.Query', () => {
+		describe('.technology', () => {
+			assert(technologyResolvers.Query?.technology instanceof Function);
+			const RESOLVER_FN = technologyResolvers.Query?.technology;
 
-			const subject = await apolloServer.executeOperation<TechnologyQuery>({
-				query,
-				variables: { id: MOCK_TECHNOLOGIES[1].id },
+			it('has correct implementation', () => {
+				expect(RESOLVER_FN).toBeDefined();
+				expect(RESOLVER_FN).toBeInstanceOf(Function);
 			});
 
-			assert(subject.body.kind === 'single');
-			expect(subject.body.singleResult.errors).toBeUndefined();
-			expect(subject.body.singleResult.data?.technology).toEqual([
-				MOCK_TECHNOLOGIES[1],
-			]);
-			expect(getEntry).toHaveBeenCalledWith(MOCK_TECHNOLOGIES[1].id);
-		});
-	});
+			describe('when called with id', () => {
+				describe('and id exists', () => {
+					const MOCK_REQUESTED_ID = 'MOCK_REQUESTED_ID';
 
-	describe('mutation createTechnology', () => {
-		it('creates a technology with the given content', async () => {
-			const query = gql`
-				mutation CreateTechnologyMutation(
-					$displayName: String!
-					$description: String
-					$url: String
-				) {
-					createTechnology(
-						displayName: $displayName
-						description: $description
-						url: $url
-					) {
-						id
-						displayName
-						description
-						url
-					}
-				}
-			`;
+					const EXPECTED_RESULT = [
+						{
+							description: 'MOCK_DESCRIPTION_1',
+							displayName: 'MOCK_DISPLAY_NAME_1',
+							id: 'MOCK_ID_1',
+							url: 'MOCK_URL_1',
+						},
+					];
 
-			const subject: GraphQLResponse =
-				await apolloServer.executeOperation<TechnologyQuery>({
-					query,
-					variables: MOCK_TECHNOLOGIES[0],
+					let result: Maybe<Maybe<ResolverTypeWrapper<Technology>>[]>;
+
+					beforeAll(async () => {
+						MOCK_TECHNOLOGY_MODEL_STATIC.get.mockResolvedValue(MOCK_ENTRY_1);
+						result = await RESOLVER_FN(
+							MOCK_PARENT_QUERY,
+							{ id: MOCK_REQUESTED_ID },
+							MOCK_CONTEXT,
+							MOCK_RESOLVE_INFO
+						);
+					});
+
+					afterAll(() => {
+						MOCK_TECHNOLOGY_MODEL_STATIC.get.mockReset();
+					});
+
+					it('calls TechnologyModel.get method once with expected argument', () => {
+						expect(MOCK_TECHNOLOGY_MODEL_STATIC.get).toHaveBeenCalledTimes(1);
+						expect(MOCK_TECHNOLOGY_MODEL_STATIC.get).toHaveBeenCalledWith(
+							MOCK_REQUESTED_ID
+						);
+					});
+
+					it('returns expected result', () => {
+						expect(result).toBeInstanceOf(Array);
+						expect(result).toHaveLength(1);
+
+						expect(result).toEqual(EXPECTED_RESULT);
+					});
 				});
 
-			assert(subject.body.kind === 'single');
-			expect(subject.body.singleResult.errors).toBeUndefined();
-			expect(subject.body.singleResult.data?.createTechnology).toEqual(
-				MOCK_TECHNOLOGIES[0]
-			);
-			expect(createEntry).toHaveBeenCalledWith('technology', {
-				fields: MOCK_CONTENTFUL_ENTRIES.items[0].fields,
+				//TODO: fix implementation
+				// describe('and id does not exist', () => {
+
+				// })
+			});
+
+			describe('when called without id', () => {
+				describe('and items found: ', () => {
+					const EXPECTED_RESULT = [
+						{
+							description: 'MOCK_DESCRIPTION_1',
+							displayName: 'MOCK_DISPLAY_NAME_1',
+							id: 'MOCK_ID_1',
+							url: 'MOCK_URL_1',
+						},
+						{
+							description: 'MOCK_DESCRIPTION_2',
+							displayName: 'MOCK_DISPLAY_NAME_2',
+							id: 'MOCK_ID_2',
+							url: 'MOCK_URL_2',
+						},
+					];
+
+					let result: Maybe<Maybe<ResolverTypeWrapper<Technology>>[]>;
+
+					beforeAll(async () => {
+						const MOCK_RETURN = [
+							MOCK_ENTRY_1,
+							MOCK_ENTRY_2
+						]
+						MOCK_TECHNOLOGY_MODEL_STATIC.getAll.mockResolvedValue(MOCK_RETURN);
+						result = await RESOLVER_FN(
+							MOCK_PARENT_QUERY,
+							{},
+							MOCK_CONTEXT,
+							MOCK_RESOLVE_INFO
+						);
+					});
+
+					afterAll(() => {
+						MOCK_TECHNOLOGY_MODEL_STATIC.getAll.mockReset();
+					});
+
+					it('calls TechnologyModel.getAll method once', () => {
+						expect(MOCK_TECHNOLOGY_MODEL_STATIC.getAll).toHaveBeenCalledTimes(1);
+					});
+
+					it('returns expected result', () => {
+						expect(result).toBeInstanceOf(Array);
+						expect(result).toHaveLength(EXPECTED_RESULT.length);
+						expect(result).toEqual(EXPECTED_RESULT);
+					});
+				});
 			});
 		});
 	});
 
-	describe('mutation updateTechnology', () => {
-		it('updates the technology with the given content', async () => {
-			const query = gql`
-				mutation UpdateTechnologyMutation(
-					$id: ID!
-					$displayName: String
-					$description: String
-					$url: String
-				) {
-					updateTechnology(
-						id: $id
-						displayName: $displayName
-						description: $description
-						url: $url
-					) {
-						id
-						displayName
-						description
-						url
-					}
+	describe('.Mutation', () => {
+		describe('.createTechnology', () => {
+			assert(technologyResolvers.Mutation?.createTechnology instanceof Function);
+			const RESOLVER_FN = technologyResolvers.Mutation?.createTechnology;
+
+			
+			it('has correct implementation', () => {
+				expect(RESOLVER_FN).toBeDefined();
+				expect(RESOLVER_FN).toBeInstanceOf(Function);
+			});
+
+			describe('when called', () => {
+				const MOCK_CREATE_TECHNOLOGY_ARGS: RequireFields<MutationCreateTechnologyArgs, "displayName"> = {
+					displayName: 'MOCK_DISPLAY_NAME',
+					description: 'MOCK_DESCRIPTION',
+					url: 'MOCK_URL'
 				}
-			`;
 
-			const subject: GraphQLResponse =
-				await apolloServer.executeOperation<TechnologyQuery>({
-					query,
-					variables: MOCK_TECHNOLOGIES[1],
-				});
+				let result: Technology | null;
 
-			assert(subject.body.kind === 'single');
-			expect(subject.body.singleResult.errors).toBeUndefined();
-			expect(subject.body.singleResult.data?.updateTechnology).toEqual(
-				MOCK_TECHNOLOGIES[1]
-			);
+				const EXPECTED_RESULT: Technology = {
+					description: 'MOCK_DESCRIPTION_1',
+					displayName: 'MOCK_DISPLAY_NAME_1',
+					id: 'MOCK_ID_1',
+					url: 'MOCK_URL_1'
+				}
+
+				beforeAll(async () => {
+					MOCK_TECHNOLOGY_MODEL_STATIC.create.mockResolvedValue(MOCK_ENTRY_1);
+					result = await RESOLVER_FN(MOCK_PARENT_MUTATION, MOCK_CREATE_TECHNOLOGY_ARGS, MOCK_CONTEXT, MOCK_RESOLVE_INFO);
+				})
+				afterAll( () => {
+					MOCK_TECHNOLOGY_MODEL_STATIC.create.mockReset();
+				})
+
+				it('calls TechnologyModel.create method once with expected argument', () => {
+					expect(MOCK_TECHNOLOGY_MODEL_STATIC.create).toHaveBeenCalledTimes(1);
+					expect(MOCK_TECHNOLOGY_MODEL_STATIC.create).toHaveBeenCalledWith(MOCK_CREATE_TECHNOLOGY_ARGS);
+				})
+
+				it('returns expected result', () => {
+					expect(result).toEqual(EXPECTED_RESULT);
+				})
+			})
+		});
+	});
+
+	describe('.Mutation', () => {
+		describe('.updateTechnology', () => {
+			assert(technologyResolvers.Mutation?.updateTechnology instanceof Function);
+			const RESOLVER_FN = technologyResolvers.Mutation?.updateTechnology;
+
+			it('has correct implementation', () => {
+				expect(RESOLVER_FN).toBeDefined();
+				expect(RESOLVER_FN).toBeInstanceOf(Function);
+			});
+
+			describe('when called', () => {
+				const MOCK_UPDATED_FIELDS = {					
+					description: 'MOCK_DESCRIPTION',
+					displayName: 'MOCK_DISPLAY_NAME',
+					url: 'MOCK_URL',
+				}
+				const MOCK_ID_ARG = 'MOCK_ID';
+				const MOCK_UPDATE_TECHNOLOGY_ARGS: RequireFields<MutationUpdateTechnologyArgs, "id"> = {
+					id: MOCK_ID_ARG, 
+					...MOCK_UPDATED_FIELDS
+				}
+
+				let result: Technology | null;
+
+				const EXPECTED_RESULT: Technology = {
+					description: 'MOCK_DESCRIPTION_1',
+					displayName: 'MOCK_DISPLAY_NAME_1',
+					id: 'MOCK_ID_1',
+					url: 'MOCK_URL_1'
+				}
+
+				beforeAll(async () => {
+					MOCK_TECHNOLOGY_MODEL_STATIC.update.mockResolvedValue(MOCK_ENTRY_1);
+					result = await RESOLVER_FN(MOCK_PARENT_MUTATION, MOCK_UPDATE_TECHNOLOGY_ARGS, MOCK_CONTEXT, MOCK_RESOLVE_INFO);
+				})
+				afterAll( () => {
+					MOCK_TECHNOLOGY_MODEL_STATIC.update.mockReset();
+				})
+
+				it('calls TechnologyModel.update method once with expected argument', () => {
+					expect(MOCK_TECHNOLOGY_MODEL_STATIC.update).toHaveBeenCalledTimes(1);
+					expect(MOCK_TECHNOLOGY_MODEL_STATIC.update).toHaveBeenCalledWith(MOCK_ID_ARG, MOCK_UPDATED_FIELDS);
+				})
+
+				it('returns expected result', () => {
+					expect(result).toEqual(EXPECTED_RESULT);
+				})
+			})
 		});
 	});
 });
