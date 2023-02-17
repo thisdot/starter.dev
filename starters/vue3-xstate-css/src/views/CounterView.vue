644a49b6adebbe1c30d035db4941e5bcb6cc5847
LindaKat <lindakt16@gmail.com>
2023-02-17 17:59:43 -0600

    fix: named machine for visualizer

--- starters/vue3-xstate-css/src/views/CounterView.vue
+++ starters/vue3-xstate-css/src/views/CounterView.vue
@@ -6,6 +6,7 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 
 const counterMachine = createMachine(
   {
+    id: 'Counter',
     predictableActionArguments: true,
     schema: {
       context: {} as { count: number },
