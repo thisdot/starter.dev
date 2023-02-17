0d62f3e76b8fb8c4ee0f417ab131f5e9660f456a
jdwilkin4 <jwilkin4@hotmail.com>
2023-02-17 11:45:01 -0800

    chore: setting up sequelize

--- starters/nestjs-bullmq-postgresql/src/app.module.ts
+++ starters/nestjs-bullmq-postgresql/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import 'dotenv/config';
+import { Dialect } from 'sequelize';
 
 @Module({
-	imports: [],
+	imports: [
+		SequelizeModule.forRoot({
+			dialect: process.env.dialect as Dialect,
+			host: process.env.host,
+			port: Number(process.env.port),
+			username: process.env.username,
+			password: process.env.password,
+			database: process.env.database,
+			models: [],
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
