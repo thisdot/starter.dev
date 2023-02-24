47b9dc268f0ef1fd50d7c1c230be7c26fc8296b5
Mattia Magi <mattia.magi91@gmail.com>
2023-02-24 11:12:14 +0100

    solidstart-fix: clean up code

--- starters/solidstart-tanstackquery-tailwind-modules/src/root.tsx
+++ starters/solidstart-tanstackquery-tailwind-modules/src/root.tsx
@@ -27,9 +27,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>
-          Solid Start - Tanstack Query - Tailwind CSS - CSS Modules Kit
-        </Title>
+        <Title>Solid Start</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
