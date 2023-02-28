352194132f658529c59e0d248a04695c1b81bd2c
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-02-28 17:34:31 +0300

    [express-apollo-prisma] Missing and excess files#1149 - Remove hello query and resolvers

--- starters/express-apollo-prisma/src/graphql/schema/index.ts
+++ starters/express-apollo-prisma/src/graphql/schema/index.ts
@@ -2,11 +2,10 @@ import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { GraphQLSchema } from 'graphql';
 
-import { helloResolvers, helloTypeDefs } from './hello';
 import { technologyResolvers, technologyTypeDefs } from './technology';
 
-const typeDefs = mergeTypeDefs([helloTypeDefs, technologyTypeDefs]);
+const typeDefs = mergeTypeDefs([technologyTypeDefs]);
 
-const resolvers = mergeResolvers([helloResolvers, technologyResolvers]);
+const resolvers = mergeResolvers([technologyResolvers]);
 
 export const schema: GraphQLSchema = makeExecutableSchema({ typeDefs, resolvers });
