56dd63dc32ee60648b1457700cfdcbd6a65e38a0
jdwilkin4 <jwilkin4@hotmail.com>
2023-03-01 16:09:14 -0800

    fix: removing duplication for counter test

--- starters/next12-chakra-ui/__tests__/Counter.test.tsx
+++ starters/next12-chakra-ui/__tests__/Counter.test.tsx
@@ -2,6 +2,26 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import CounterExample from "../src/pages/counter-example";
 import "@testing-library/jest-dom";
 
+type Params = {
+  btnText: string;
+  testValue1: string;
+  testValue2: string;
+};
+
+const handleCounter = ({ btnText, testValue1, testValue2 }: Params) => {
+  render(<CounterExample />);
+
+  const currentCount = screen.getByText("0");
+  const button = screen.getByText(btnText);
+  expect(currentCount).toHaveTextContent("0");
+
+  fireEvent.click(button);
+  expect(currentCount).toHaveTextContent(testValue1);
+
+  fireEvent.click(button);
+  expect(currentCount).toHaveTextContent(testValue2);
+};
+
 describe("Counter Example page", () => {
   it("renders a heading", () => {
     render(<CounterExample />);
@@ -15,35 +35,17 @@ describe("Counter Example page", () => {
 });
 
 describe("Increment Counter", () => {
-  it("should increase by 1 when clicking the button", () => {
-    render(<CounterExample />);
-
-    const currentCount = screen.getByText("0");
-    const button = screen.getByText("Increment");
-    expect(currentCount).toHaveTextContent("0");
-
-    fireEvent.click(button);
-    expect(currentCount).toHaveTextContent("1");
-
-    fireEvent.click(button);
-    expect(currentCount).toHaveTextContent("2");
-  });
+  it("should increase by 1 when clicking the button", () =>
+    handleCounter({ btnText: "Increment", testValue1: "1", testValue2: "2" }));
 });
 
 describe("Decrement Counter", () => {
-  it("should decrease by 1 when clicking the button", () => {
-    render(<CounterExample />);
-
-    const currentCount = screen.getByText("0");
-    const button = screen.getByText("Decrement");
-    expect(currentCount).toHaveTextContent("0");
-
-    fireEvent.click(button);
-    expect(currentCount).toHaveTextContent("-1");
-
-    fireEvent.click(button);
-    expect(currentCount).toHaveTextContent("-2");
-  });
+  it("should decrease by 1 when clicking the button", () =>
+    handleCounter({
+      btnText: "Decrement",
+      testValue1: "-1",
+      testValue2: "-2",
+    }));
 });
 
 describe("Reset Counter", () => {
