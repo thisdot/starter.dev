6a705bb769a387f1dd6669d26632065f43bf22cd
Chris Trzesniewski <k.trzesniewski@gmail.com>
2023-03-03 14:19:20 +0100

    fix

--- scripts/validate-starter-kits-json.js
+++ scripts/validate-starter-kits-json.js
@@ -1,16 +1,19 @@
 const simpleGit = require('simple-git');
 const path = require('path');
+const { exec } = require('child_process');
 const repoRoot = path.resolve(__dirname, '..');
 const g = simpleGit(repoRoot);
+const { promisify } = require('util')
 
-main()
+main();
 
 async function main() {
-  const [starterKitsJsonDiff] = await g.diff(['starter-kits.json'])
+  await promisify(exec)('yarn generate-starter-kits')
+  const [starterKitsJsonDiff] = await g.diff(['starter-kits.json']);
 
-  if(starterKitsJsonDiff) {
-    console.error('starter-kits.json file is not up to date. Please run `yarn generate-starter-kits` to regenerate the file')
-    process.exit(1)
+  if (starterKitsJsonDiff) {
+    console.error('starter-kits.json file is not up to date. Please run `yarn generate-starter-kits` to regenerate the file');
+    process.exit(1);
   }
 }
 
