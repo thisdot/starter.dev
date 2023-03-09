523569c7b42a96beb68236ae56e0cb6a8242a12f
Balázs Tápai <balazs.tapai1990@gmail.com>
2023-03-09 12:56:21 +0100

    feat: set up deployment workflow, use production url

--- packages/create-starter/src/metrics.ts
+++ packages/create-starter/src/metrics.ts
@@ -1,7 +1,10 @@
 import fetch from "node-fetch";
 
 // TODO: #790 hit the deployed endpoint
-const METRICS_TRACK_URL = 'https://05a78mw2c1.execute-api.us-east-1.amazonaws.com/track';
+
+// This is the production metircs-tracker url. For the development url please use the development url for dev work:
+// const METRICS_TRACK_URL = 'https://05a78mw2c1.execute-api.us-east-1.amazonaws.com/track'; // <-- dev url
+const METRICS_TRACK_URL = 'https://47ix8fys9k.execute-api.us-east-1.amazonaws.com/track';
 export async function trackSelectedKit(kit: string): Promise<void> {
   try {
     await fetch(METRICS_TRACK_URL, {
