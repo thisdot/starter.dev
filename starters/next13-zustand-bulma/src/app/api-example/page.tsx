8a27ee202f4a683613e483d6c312c64ce272361b
Adam L Barrett <adamlbarrett@hotmail.com>
2023-03-10 13:16:08 -0600

    feat(Next13): add starter routes

---
+++ starters/next13-zustand-bulma/src/app/api-example/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function ApiExamplePage() {
+  return (
+    <main className={styles.main}>
+      <h1>API Example</h1>
+    </main>
+  );
+}
---
+++ starters/next13-zustand-bulma/src/app/counter/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function CounterPage() {
+  return (
+    <main className={styles.main}>
+      <h1>Counter Example</h1>
+    </main>
+  );
+}
