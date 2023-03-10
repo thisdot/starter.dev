0ab6d8e87606c7ecb4aa35d74fc9849cb071a6ae
Adam L Barrett <adamlbarrett@hotmail.com>
2023-03-10 13:08:30 -0600

    feat(Next13): Add Styles (Bulma+SCSS) to Next 13

--- starters/next13-zustand-bulma/src/app/counter/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './page.module.css';
-
-export default function CounterPage() {
-  return (
-    <main className={styles.main}>
-      <h1>Counter Example</h1>
-    </main>
-  );
-}
