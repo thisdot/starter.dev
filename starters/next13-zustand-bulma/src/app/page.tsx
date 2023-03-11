b7453959b1172227ec05be72437fba30371df97a
Adam L Barrett <adamlbarrett@hotmail.com>
2023-03-10 19:26:32 -0600

    feat(next13): change title - PR feedback

--- starters/next13-zustand-bulma/src/app/page.tsx
+++ starters/next13-zustand-bulma/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <h1 className={styles.heading}>Create Next app with React Query and Tailwind</h1>
+        <h1 className={styles.heading}>Next.js 13 App with Zustand and Bulma</h1>
         <ul className={styles.list}>
           <li>
             <a href="/counter">See Counter example component</a>
