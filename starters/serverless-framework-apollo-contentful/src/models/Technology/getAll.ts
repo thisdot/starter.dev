9e1475d3a677480c3144ba75ad67a14c6a835d38
Maarten Bicknese <maarten.bicknese@thisdot.co>
2023-03-14 12:12:15 +0100

    feat: introduce pagination to getAll

--- starters/serverless-framework-apollo-contentful/src/models/Technology/getAll.ts
+++ starters/serverless-framework-apollo-contentful/src/models/Technology/getAll.ts
@@ -1,17 +1,20 @@
 import { getEnvironment } from '../../utils/contentful';
 import TechnologyModel from './TechnologyModel';
 
-export default async function getAll() {
+export default async function getAll(limit?: number | null, skip?: number | null) {
 	try {
 		const environment = await getEnvironment();
 		const entries = await environment.getEntries({
 			content_type: 'technology',
+			limit: limit || 100,
+			skip: skip || 0,
+			order: 'sys.createdAt',
 		});
 
 		return entries.items.map((entry) => new TechnologyModel(entry));
 	} catch (e) {
 		console.log('======= something went wrong fetch =======');
-		console.log(e);
+		console.log(String(e));
 		throw e;
 	}
 }
