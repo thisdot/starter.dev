8c40a0af7e900122c9d06ff7d2de5d219388ccc8
Adam L Barrett <adam.barrett@thisdot.co>
2023-03-14 14:11:42 -0600

    feat(Next13): Add Styles (Bulma+SCSS) to Next 13 (#1173)

--- starters/next13-zustand-bulma/src/app/layout.tsx
+++ starters/next13-zustand-bulma/src/app/layout.tsx
@@ -1,18 +1,39 @@
-import './globals.css'
+import './globals.scss';
+import { Open_Sans } from 'next/font/google';
+
+const opensans = Open_Sans({ subsets: ['latin'], weight: 'variable' });
+
+// Static metadata
+export const metadata = {
+  title: {
+    default: 'next13-zustand-bulma',
+    template: '%s | next13-zustand-bulma',
+  },
+  description: 'next13-zustand-bulma starter kit - starter.dev',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className={opensans.className} lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="columns">
+          <div className="column" />
+          <div className="column is-two-thirds">{children}</div>
+          <div className="column" />
+        </div>
+      </body>
     </html>
-  )
+  );
 }
