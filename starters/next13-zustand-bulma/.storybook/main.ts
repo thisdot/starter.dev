b75f26278be1f5ed3dbe69247dad187ab4b2d142
Adam L Barrett <adam.barrett@thisdot.co>
2023-03-14 14:26:22 -0600

    feat(next13-zustand-bulma): add storybook (#1183)

---
+++ starters/next13-zustand-bulma/.storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
