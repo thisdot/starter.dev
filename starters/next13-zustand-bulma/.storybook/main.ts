6193c2573ac2e5dc8b51fb226a5165f2a813effe
Adam L Barrett <adamlbarrett@hotmail.com>
2023-03-14 14:08:32 -0600

    PR feedback: remove example stories and add a11y plugin

--- starters/next13-zustand-bulma/.storybook/main.ts
+++ starters/next13-zustand-bulma/.storybook/main.ts
@@ -2,9 +2,10 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
+    '@storybook/addon-a11y',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-links',
   ],
   framework: {
     name: '@storybook/nextjs',
