b9f23f2f2d9b126dd33ae8ffdf3d02fc0b8e145f
Linda Thompson <lindakt16@gmail.com>
2023-03-31 12:49:27 -0500

    [Vue3-XState] README documentation (#1196)
    * feat: start process on readme
    
    * feat: finish first draft of readme
    
    * fix: cleanup of readme round 1
    
    * fix: try to fix command list
    
    * fix: decided to separate tests for consistency, update readme to reflect
    
    * feat: link to setup wiki page and remove other setup steps

--- starters/vue3-xstate-css/cypress.config.ts
+++ starters/vue3-xstate-css/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   viewportHeight: 720,
   viewportWidth: 1280,
   component: {
-    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+    specPattern: 'src/tests/*.{cy,spec}.{js,ts,jsx,tsx}',
     devServer: {
       framework: 'vue',
       bundler: 'vite',
