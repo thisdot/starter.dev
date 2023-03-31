dce5cc97f2c90cdadc27ad80a8775b264c761068
jdwilkin4 <jwilkin4@hotmail.com>
2023-03-31 16:25:43 -0700

    chore: setting up CORS

--- starters/nestjs-bullmq-postgresql/src/main.ts
+++ starters/nestjs-bullmq-postgresql/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: process.env.origins_urls ? process.env.origins_urls.split(',') : false,
+		// methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		// optionsSuccessStatus: 204,
+	});
+
 	await app.listen(3000);
 }
 bootstrap();
