fe8d52a60b9335896b8751286ec62ff9ea043d50
jdwilkin4 <jwilkin4@hotmail.com>
2023-04-21 15:53:51 -0700

    docs: setting up API docs using swagger

--- starters/nestjs-bullmq-postgresql/src/main.ts
+++ starters/nestjs-bullmq-postgresql/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+	const config = new DocumentBuilder()
+		.setTitle('NestJS, Bull MQ and PostgreSQL starter kit API')
+		.setDescription('NestJS starter kit API with CRUD functionality')
+		.setVersion('1.0')
+		.build();
+
 	const app = await NestFactory.create(AppModule);
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
--- starters/nestjs-bullmq-postgresql/src/technology/dto/create-technology.dto.ts
+++ starters/nestjs-bullmq-postgresql/src/technology/dto/create-technology.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateTechnologyDto {
+	@ApiProperty({
+		type: String,
+		description: 'This is a required property',
+	})
 	displayName: string;
+	@ApiProperty({
+		type: String,
+		description: 'This is a required property',
+	})
 	description: string;
+	@ApiProperty({
+		type: String,
+		description: 'This is a required property',
+	})
 	url: string;
 }
