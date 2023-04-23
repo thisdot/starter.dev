458e8d0a939c775e128099d885bef869c629d9eb
jesus padron <jesuspadron4497@gmail.com>
2023-04-23 10:23:28 -0400

    feat: fetch example logic

--- starters/nuxt3-pinia-vuetify/pages/index.vue
+++ starters/nuxt3-pinia-vuetify/pages/index.vue
@@ -3,8 +3,20 @@
 		<NuxtLink to="/counter" class="text-subtitle-1 text-blue-darken-2">
 			See Counter example component
 		</NuxtLink>
-		<NuxtLink to="/api-example" class="text-subtitle-1 text-blue-darken-2 mt-4">
+		<NuxtLink to="/greet" class="text-subtitle-1 text-blue-darken-2 mt-4">
 			See Fetch API example component
 		</NuxtLink>
 	</div>
 </template>
+
+<script setup lang="ts">
+useHead({
+	title: 'Nuxt 3, Pinia and Vuetify',
+	meta: [
+		{
+			name: 'Nuxt 3, Pinia and Vuetify',
+			content: 'Nuxt 3, Pinia and Vuetify kit',
+		},
+	],
+});
+</script>
