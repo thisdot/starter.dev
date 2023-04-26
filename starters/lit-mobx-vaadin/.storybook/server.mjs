b0eed4cc14067f1787f219fd9379047a216758f2
Jamie Kuppens <jamie.kuppens@thisdot.co>
2023-04-26 08:55:37 -0700

    feat: add storybook

--- starters/lit-mobx-vaadin/.storybook/server.mjs
+++ starters/lit-mobx-vaadin/.storybook/server.mjs
@@ -3,6 +3,6 @@ import baseConfig from '../web-dev-server.config.mjs';
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   ...baseConfig,
-  open: '/',
+  appIndex: './index.storybook.html',
   plugins: [storybookPlugin({ type: 'web-components' }), ...baseConfig.plugins],
 });
