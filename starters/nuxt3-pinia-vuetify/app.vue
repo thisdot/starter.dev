101d4220bab611a82885347654343b12145756f7
Jesus Padron <jesuspadron4497@gmail.com>
2023-05-01 17:44:15 -0400

    [Nuxt 3 - Pinia - Vuetify] Fix: sanity check (#1234)
    * fix: sanity check
    
    * fix: feedback

--- starters/nuxt3-pinia-vuetify/app.vue
+++ starters/nuxt3-pinia-vuetify/app.vue
@@ -1,25 +1,19 @@
+<script lang="ts" setup>
+const Route = useRoute();
+</script>
+
 <template>
 	<div
 		class="v-theme--customTheme d-flex flex-column justify-center align-center mt-5"
 	>
-		<header class="bg-blue-darken-2 w-50 pa-4 text-center rounded mb-5">
-			<h1 class="text-h4 font-weight-bold text-white">
-				Nuxt 3, Pinia and Vuetify Starter kit
-			</h1>
-		</header>
-
 		<NuxtPage />
 
 		<NuxtLink
 			v-if="Route.path !== '/'"
 			to="/"
-			class="text-h6 text-primary mt-10"
+			class="text-h6 text-primary mt-3"
 		>
 			Return Home
 		</NuxtLink>
 	</div>
 </template>
-
-<script lang="ts" setup>
-const Route = useRoute();
-</script>
--- starters/nuxt3-pinia-vuetify/pages/index.vue
+++ starters/nuxt3-pinia-vuetify/pages/index.vue
@@ -1,14 +1,3 @@
-<template>
-	<div class="d-flex flex-column justify-center align-center mt-5">
-		<NuxtLink to="/counter" class="text-subtitle-1 text-blue-darken-2">
-			See Counter example component
-		</NuxtLink>
-		<NuxtLink to="/greet" class="text-subtitle-1 text-blue-darken-2 mt-4">
-			See Fetch API example component
-		</NuxtLink>
-	</div>
-</template>
-
 <script setup lang="ts">
 useHead({
 	title: 'Nuxt 3, Pinia and Vuetify',
@@ -20,3 +9,21 @@ useHead({
 	],
 });
 </script>
+
+<template>
+	<main class="w-50">
+		<header class="bg-blue-darken-2 pa-4 text-center rounded mb-5">
+			<h1 class="text-h5 font-weight-bold text-white">
+				Nuxt 3, Pinia and Vuetify Starter kit
+			</h1>
+		</header>
+		<div class="d-flex flex-column justify-center align-center mt-4">
+			<NuxtLink to="/counter" class="text-subtitle-1 text-blue-darken-2">
+				See Counter example component
+			</NuxtLink>
+			<NuxtLink to="/greet" class="text-subtitle-1 text-blue-darken-2 mt-2">
+				See Fetch example component
+			</NuxtLink>
+		</div>
+	</main>
+</template>
