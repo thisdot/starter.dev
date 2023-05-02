7d67db4d2bf92d828b34e5dfdad38fbc8a254deb
William Takashi Mimura <william.mimura@thisdot.co>
2023-05-02 13:25:29 -0700

    chore: run prettier on index.js
    The real objective of this commit is to trigger the gh action,
    possibly fixed in fedc4daa36a071dec6d1173df68b03d8c217df4e

--- packages/metrics-handler/index.js
+++ packages/metrics-handler/index.js
@@ -6,7 +6,8 @@ function createOkResponse(body) {
 }
 
 const STARTER_KITS_JSON_URL = 'https://raw.githubusercontent.com/thisdot/starter.dev/main/starter-kits.json';
-const GOOGLE_MEASUREMENT_PROTOCOL_ENDPOINT = (measurement_id, api_secret) => `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`;
+const GOOGLE_MEASUREMENT_PROTOCOL_ENDPOINT = (measurement_id, api_secret) =>
+  `https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`;
 
 module.exports.handler = async (event) => {
   const MEASUREMENT_ID = process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID;
@@ -20,35 +21,30 @@ module.exports.handler = async (event) => {
   const starterKitsRequest = await fetch(STARTER_KITS_JSON_URL);
   if (!starterKitsRequest.ok) {
     console.log('Could not fetch starter kits for validation, tracking aborted');
-    return createOkResponse('Could not fetch starter kits for validation, tracking aborted')
+    return createOkResponse('Could not fetch starter kits for validation, tracking aborted');
   }
 
   const starterKitsJSON = await starterKitsRequest.json();
-  const starterKits = new Set(
-    typeof starterKitsJSON === 'object' && starterKitsJSON !== null
-      ? Object.keys(starterKitsJSON)
-      : []
-  )
+  const starterKits = new Set(typeof starterKitsJSON === 'object' && starterKitsJSON !== null ? Object.keys(starterKitsJSON) : []);
 
   if (!starterKits.has(selectedStarterKit)) {
     console.log('Invalid starter kit, tracking aborted');
     return createOkResponse('Invalid starter kit, tracking aborted');
   }
 
-  const trackRequest = await fetch(
-    GOOGLE_MEASUREMENT_PROTOCOL_ENDPOINT(MEASUREMENT_ID, API_SECRET),
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        // The client-id is hardcoded to be a valid client id.
-        client_id: '1443243224.1678357674',
-        events: [{
+  const trackRequest = await fetch(GOOGLE_MEASUREMENT_PROTOCOL_ENDPOINT(MEASUREMENT_ID, API_SECRET), {
+    method: 'POST',
+    body: JSON.stringify({
+      // The client-id is hardcoded to be a valid client id.
+      client_id: '1443243224.1678357674',
+      events: [
+        {
           name: 'create_starter',
-          params: { selectedStarterKit }
-        }]
-      })
-    }
-  )
+          params: { selectedStarterKit },
+        },
+      ],
+    }),
+  });
 
   if (!trackRequest.ok) {
     console.log(`Track request returned with status ${trackRequest.status} - ${trackRequest.statusText}`);
