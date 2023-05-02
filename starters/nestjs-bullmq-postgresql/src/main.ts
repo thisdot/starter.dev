8bb3374c8716bade2a551da3d77b5d08b6744703
jdwilkin4 <jwilkin4@hotmail.com>
2023-05-02 09:15:18 -0700

    fix: removing default props

--- starters/nestjs-bullmq-postgresql/src/main.ts
+++ starters/nestjs-bullmq-postgresql/src/main.ts
@@ -6,9 +6,6 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
 		origin: process.env.origins_urls ? process.env.origins_urls.split(',') : false,
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		preflightContinue: false,
-		optionsSuccessStatus: 204,
 	});
 
 	await app.listen(3000);
