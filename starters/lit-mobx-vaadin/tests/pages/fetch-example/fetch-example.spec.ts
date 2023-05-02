a35f4cfe1d6b5f1a714105874ab12739ff73b133
Jamie Kuppens <jamie.kuppens@thisdot.co>
2023-05-02 15:33:48 -0700

    test: add fetch tests

---
+++ starters/lit-mobx-vaadin/tests/pages/fetch-example/fetch-example.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:8000/api-example');
+});
+
+test('be able to fetch', async ({ page }) => {
+  await expect(
+    page.getByText('Message: Hello, lit-mobx-vaadin starter.dev!'),
+  ).toBeVisible();
+});
