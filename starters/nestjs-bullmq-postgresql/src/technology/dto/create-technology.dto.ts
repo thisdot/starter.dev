c05994a567f581f7e64cb29fb4e9a46e8d9289a8
jdwilkin4 <jwilkin4@hotmail.com>
2023-05-02 10:16:21 -0700

    chore: applying code review comments

--- starters/nestjs-bullmq-postgresql/src/technology/dto/create-technology.dto.ts
+++ starters/nestjs-bullmq-postgresql/src/technology/dto/create-technology.dto.ts
@@ -1,19 +1,5 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-
 export class CreateTechnologyDto {
-	@ApiProperty({
-		type: String,
-		description: 'This is a required property',
-	})
 	displayName: string;
-	@ApiProperty({
-		type: String,
-		description: 'This is a required property',
-	})
 	description: string;
-	@ApiProperty({
-		type: String,
-		description: 'This is a required property',
-	})
 	url: string;
 }
