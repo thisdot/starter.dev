1f640b2c9d74c6dedd9b52e56ab056b0c88407d2
Jamie Kuppens <jamie.kuppens@thisdot.co>
2023-05-03 13:43:23 -0700

    docs: add counter and fetch stories

---
+++ starters/lit-mobx-vaadin/stories/components/counter/counter.stories.ts
@@ -0,0 +1,24 @@
+import { html, TemplateResult } from 'lit';
+
+import '../../../src/components/counter/counter.js';
+
+export default {
+  title: 'Counter',
+  component: 'td-counter',
+  argTypes: {},
+};
+
+interface Story<T> {
+  (args: T): TemplateResult;
+  args?: Partial<T>;
+  argTypes?: Record<string, unknown>;
+}
+
+interface ArgTypes {
+  title?: string;
+}
+
+const Template: Story<ArgTypes> = () => html`<td-counter></td-counter>`;
+
+export const Counter = Template.bind({});
+Counter.args = {};
