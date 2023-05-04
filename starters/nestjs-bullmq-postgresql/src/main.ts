4e3f957323cffc8a4a8a130d4f9b7002209b7594
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2023-05-03 21:20:08 -0700

    [nestjs-bullmq-postgres]: setting up API docs using swagger (#1215)
    * chore: setting up sequelize
    
    * chore: adding import for technology model
    
    * feat: adding technology files
    
    * fix: removing postgres file
    
    * fix: updating credentials in env example
    
    * docs: setting up API docs using swagger
    
    * fix: pinning dependencies
    
    * [nestjs-bullmq-postgres]: setting up CORS (#1209)
    
    * chore: setting up CORS
    
    * fix: pinned dependencies
    
    * fix: removing default props
    
    * chore: applying code review comments
    
    * fix: removing extra start commands

--- starters/nestjs-bullmq-postgresql/src/main.ts
+++ starters/nestjs-bullmq-postgresql/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
+	const config = new DocumentBuilder()
+		.setTitle('NestJS, Bull MQ and PostgreSQL starter kit API')
+		.setDescription('NestJS starter kit API with CRUD functionality')
+		.setVersion('1.0')
+		.build();
+
 	const app = await NestFactory.create(AppModule);
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
 	app.enableCors({
 		origin: process.env.origins_urls ? process.env.origins_urls.split(',') : false,
 	});
