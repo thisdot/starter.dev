21375b9abfe86ff573b9a8479ee859437b293b58
LindaKat <lindakt16@gmail.com>
2023-05-19 16:50:35 -0500

    fix: revert home route to use dynamic import

--- starters/vue3-xstate-css/src/router/index.ts
+++ starters/vue3-xstate-css/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,7 @@ const router = createRouter({
 		{
 			path: '/',
 			name: 'home',
-			component: HomeView,
+			component: () => import('../views/HomeView.vue'),
 		},
 		{
 			path: '/counter',
