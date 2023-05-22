c072b511ca2ee7737e5ee67691fc2b4ad0ede5d5
jdwilkin4 <jwilkin4@hotmail.com>
2023-05-22 11:09:07 -0700

    refactor: updating nuxt config

--- starters/nuxt3-pinia-vuetify/nuxt.config.ts
+++ starters/nuxt3-pinia-vuetify/nuxt.config.ts
@@ -2,11 +2,7 @@
 export default defineNuxtConfig({
 	modules: ['@pinia/nuxt'],
 	pinia: {
-		autoImports: [
-			// automatically imports `defineStore`
-			'defineStore', // import { defineStore } from 'pinia'
-			['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-		],
+		autoImports: ['defineStore', ['defineStore', 'definePiniaStore']],
 	},
 	css: ['vuetify/lib/styles/main.sass'],
 	build: {
