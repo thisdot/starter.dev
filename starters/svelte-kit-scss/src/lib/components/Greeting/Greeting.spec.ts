4a0c0784334e44a6a11ddda077506ae96b78596a
Ihar Dziamidau <31627738+ihardz@users.noreply.github.com>
2023-07-17 12:45:20 +0300

    [svelte-kit-scss] jest-dom types are missing#1204 (#1298)

--- starters/svelte-kit-scss/src/lib/components/Greeting/Greeting.spec.ts
+++ starters/svelte-kit-scss/src/lib/components/Greeting/Greeting.spec.ts
@@ -1,11 +1,13 @@
 import Greeting from '$lib/components/Greeting/Greeting.svelte';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
+import '@testing-library/jest-dom';
 
 describe('Greeting Component', () => {
   test('should show title', () => {
     const message = 'from Vitest';
-    const { getByText } = render(Greeting, { message });
+    render(Greeting, { message });
+    const greeting = screen.getByText(`Message: ${message}`);
 
-    expect(() => getByText(`Hello, ${message}`));
+    expect(greeting).toBeInTheDocument();
   });
 });
