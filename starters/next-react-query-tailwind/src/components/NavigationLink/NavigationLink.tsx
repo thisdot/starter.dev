69b73ddf93156064b59a698711d1b1d4f2d30130
Ian Mungai <iansam74@gmail.com>
2023-07-20 11:58:00 +0300

    chore update to next13

--- starters/next-react-query-tailwind/src/components/NavigationLink/NavigationLink.tsx
+++ starters/next-react-query-tailwind/src/components/NavigationLink/NavigationLink.tsx
@@ -7,8 +7,12 @@ type NavigationLinkProps = {
 
 export const NavigationLink = ({ to, label }: NavigationLinkProps) => {
   return (
-    <Link href={to}>
-      <a className="underline text-blue-600 hover:text-blue-800 text-xl" role="link">{label}</a>
+    <Link
+      className="underline text-blue-600 hover:text-blue-800 text-xl"
+      role="link"
+      href={to}
+    >
+      {label}
     </Link>
   );
-}
+};
