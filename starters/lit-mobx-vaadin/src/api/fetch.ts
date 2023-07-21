109a3e281d64096fdd139b9d1b2dc94dad802942
Ignacio Falk <flakolefluk@gmail.com>
2023-07-21 11:27:35 -0400

    use const

--- starters/lit-mobx-vaadin/src/api/fetch.ts
+++ starters/lit-mobx-vaadin/src/api/fetch.ts
@@ -1,7 +1,7 @@
 const FETCH_API_URL = 'https://api.starter.dev/.netlify/functions/server/hello';
 
 export function fetchMessage(greeting: string): Promise<string> {
-	var url = new URL(FETCH_API_URL);
+	const url = new URL(FETCH_API_URL);
 	url.searchParams.append('greeting', greeting);
 	return fetch(url).then(
 		response => response.text(),
