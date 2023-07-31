e88ac93c70332fd5bd693fc715164f37189e1275
Jan Kaiser <honza.kaiser@gmail.com>
2023-07-31 16:28:03 +0200

    chore: ng update to angular 16

--- starters/angular-apollo-tailwind/src/app/app.component.spec.ts
+++ starters/angular-apollo-tailwind/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
