e878640721f4b9ec5a34505c8d273c6ef984d81b
Jan Kaiser <honza.kaiser@gmail.com>
2023-07-31 15:01:10 +0200

    chore: update storybook

--- starters/angular-apollo-tailwind/.storybook/main.js
+++ starters/angular-apollo-tailwind/.storybook/main.js
@@ -1,15 +1,19 @@
+import { dirname, join } from "path";
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions")],
+  "framework": {
+    name: getAbsolutePath("@storybook/angular"),
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
+};
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
