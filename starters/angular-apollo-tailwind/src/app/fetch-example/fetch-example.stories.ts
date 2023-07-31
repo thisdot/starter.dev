db06135be285fe2e96da46f2e5ce7bed1152a22b
Jan Kaiser <honza.kaiser@gmail.com>
2023-07-31 19:23:46 +0200

    fix: fix fetch story

--- starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.stories.ts
+++ starters/angular-apollo-tailwind/src/app/fetch-example/fetch-example.stories.ts
@@ -1,7 +1,11 @@
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-import { ApolloModule } from 'apollo-angular';
+import { Meta, applicationConfig, moduleMetadata } from '@storybook/angular';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 
 import { FetchExampleComponent } from './fetch-example.component';
+import { createApollo } from '../graphql.module';
+import { HttpLink } from 'apollo-angular/http';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 export default {
   title: 'Example/Fetch Example',
@@ -11,12 +15,21 @@ export default {
       declarations: [FetchExampleComponent],
       imports: [ApolloModule],
     }),
+    applicationConfig({
+      providers: [
+        importProvidersFrom(ApolloModule),
+        importProvidersFrom(HttpClientModule),
+        {
+          provide: APOLLO_OPTIONS,
+          useFactory: createApollo,
+          deps: [HttpLink],
+        },
+      ],
+    }),
   ],
 } as Meta;
 
-const Template: Story<FetchExampleComponent> = (
-  args: FetchExampleComponent
-) => ({
+const Template = (args: FetchExampleComponent) => ({
   props: args,
 });
 
