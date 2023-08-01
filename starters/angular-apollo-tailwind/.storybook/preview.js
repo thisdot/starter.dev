8062d78dd4e3b8f6dc5045913adba3b66f59b72e
Jan Kaiser <honza.kaiser@gmail.com>
2023-08-01 14:36:48 +0200

    chore: update storybook to 7.2

--- starters/angular-apollo-tailwind/.storybook/preview.js
+++ starters/angular-apollo-tailwind/.storybook/preview.js
@@ -1,14 +1,14 @@
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-  docs: { inlineStories: true },
-}
+  docs: { story: { inline: true } },
+};
