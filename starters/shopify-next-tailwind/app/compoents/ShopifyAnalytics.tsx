3e80e8daa57f493e1094fb8241d3d0ea75f06de6
Mark S. Shenouda <shenoudam102@gmail.com>
2023-10-20 11:26:45 +0300

    fix: remove double imports

--- starters/shopify-next-tailwind/app/compoents/ShopifyAnalytics.tsx
+++ starters/shopify-next-tailwind/app/compoents/ShopifyAnalytics.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { useShopifyCookies } from '@shopify/hydrogen-react';
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import {
 	sendShopifyAnalytics,
 	getClientBrowserParameters,
 	AnalyticsEventName,
+	useShopifyCookies,
 } from '@shopify/hydrogen-react';
 import { CurrencyCode } from '@/lib/useMoney';
 import { LanguageCode } from '@shopify/hydrogen-react/storefront-api-types';
